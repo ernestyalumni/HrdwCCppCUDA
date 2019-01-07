4bf05c3144fe99ee4a3faedcfc243df91f415692
ernestyalumni <ernestyalumni@gmail.com>
2019-01-07 03:17:03 -0800

    TimerFd complete

--- Cpp14/Time/TimerFd.h
+++ Cpp14/Time/TimerFd.h
@@ -33,8 +33,9 @@
 #include "Utilities/ErrorHandling.h"
 #include "Utilities/casts.h" // get_underlying_value
 
+#include <ostream>
 #include <sys/timerfd.h>
-#include <type_traits>
+#include <type_traits> // std::underlying_type_t
 #include <unistd.h>
 
 namespace Time
@@ -51,11 +52,31 @@ enum class ClockFlags : int
   close_on_execute = TFD_CLOEXEC
 };
 
+//------------------------------------------------------------------------------
+/// \brief enum class for all set time flags,
+/// \details The `flags` argument is either 0, to start a relative timer
+/// (`new_value.it_value` specifies a time relative to current value of the
+/// clock specified by `clockid`), or `TFD_TIMER_ABSTIME`, to start an absolute
+/// timer (`new_value.it_value` specifies an absolute time for the clock
+/// specified by `clockid`; i.e. timer will expire when value of that clock
+/// reaches value specified in `new_value.it_value`.)
+/// \ref https://linux.die.net/man/2/timerfd_settime
+//------------------------------------------------------------------------------
+enum class SetTimeFlags : int
+{
+  default_value = 0,
+  absolute_time = TFD_TIMER_ABSTIME//,
+//  cancel_on_set = TFD_TIMER_CANCEL_ON_SET
+};
+
 template <
   ClockIds ClockId = ClockIds::monotonic,
   int clock_flags =
     static_cast<std::underlying_type_t<ClockFlags>>(
-      ClockFlags::default_value)
+      ClockFlags::default_value),
+  int SetFlags =
+    static_cast<std::underlying_type_t<SetTimeFlags>>(
+      SetTimeFlags::default_value)
   >
 class TimerFd
 {
@@ -64,11 +85,34 @@ class TimerFd
     TimerFd():
       fd_{::timerfd_create(
         static_cast<std::underlying_type_t<ClockIds>>(ClockId),
-        clock_flags)}
+        clock_flags)},
+      expirations_buffer_{0},
+      new_value_{},
+      old_value_{},
+      current_value_{}
     {
       Utilities::ErrorHandling::HandleReturnValue{errno}(
         fd_,
         "create file descriptor (::timerfd_create)");
+
+      set_time<SetFlags>();
+    }
+
+    explicit TimerFd(
+      const IntervalTimerSpecification& interval_timer_specification):
+      fd_{::timerfd_create(
+        static_cast<std::underlying_type_t<ClockIds>>(ClockId),
+        clock_flags)},
+      expirations_buffer_{0},
+      new_value_{interval_timer_specification},
+      old_value_{},
+      current_value_{}
+    {
+      Utilities::ErrorHandling::HandleReturnValue{errno}(
+        fd_,
+        "create file descriptor (::timerfd_create)");
+
+      set_time<SetFlags>();
     }
 
     ~TimerFd()
@@ -76,10 +120,158 @@ class TimerFd
       Utilities::ErrorHandling::HandleClose{}(::close(fd_));
     }
 
+    template <
+      int Flags = static_cast<std::underlying_type_t<SetTimeFlags>>(
+        SetTimeFlags::default_value),
+      bool ProvideOldValue = true>
+    void set_time(const IntervalTimerSpecification& new_value)
+    {
+      new_value_ = new_value;
+      set_time<Flags, ProvideOldValue>();
+    }
+
+    //--------------------------------------------------------------------------
+    /// \brief Wraps ::timerfd_gettime
+    /// \details Gets current setting of the timer referred to by fd.
+    /// it_interval field returns interval of the timer. If both fields of this
+    /// structure are 0, then timer set to expire just once
+    /// it_value field returns amount of time until timer will next expire.
+    //--------------------------------------------------------------------------
+    const IntervalTimerSpecification get_time()
+    {
+      const int get_time_results {
+        ::timerfd_gettime(fd_, current_value_.to_itimerspec_pointer())};
+
+      Utilities::ErrorHandling::HandleReturnValue{errno}(
+        get_time_results, "get time (::timerfd_gettime)");
+
+      return current_value();
+    }
+
+    //--------------------------------------------------------------------------
+    /// \brief Assigns unsigned 8-byte integer containing number of expirations
+    /// to class data member expirations_buffer_.
+    //--------------------------------------------------------------------------
+    void read()
+    {
+      expirations_buffer_ = 0;
+
+      const ssize_t read_result {
+        ::read(fd_, &expirations_buffer_, sizeof(uint64_t))};
+
+      HandleReadOnTimerFd()(read_result);
+    }
+
+    // Accessors
+
+    const uint64_t expirations() const
+    {
+      return expirations_buffer_;
+    }
+
+    IntervalTimerSpecification new_value() const
+    {
+      return new_value_;
+    }
+
+    IntervalTimerSpecification old_value() const
+    {
+      return old_value_;
+    }
+
+    IntervalTimerSpecification current_value() const
+    {
+      return current_value_;
+    }
+
+    template <ClockIds AClockId, int some_clock_flags>
+    friend std::ostream& operator<<(std::ostream& os,
+      const TimerFd<AClockId, some_clock_flags>& tfd);
+
   private:
 
+    using HandleRead = Utilities::ErrorHandling::HandleRead;
+
+    //--------------------------------------------------------------------------
+    /// \url http://man7.org/linux/man-pages/man2/timerfd_create.2.html
+    /// \details If no timer expirations have occurred at time of read, then
+    /// call either blocks until next timer expiration, or fails with error
+    /// EAGAIN, if fd has been made nonblocking.
+    /// read fails with error EINVAL if size of supplied buffer less than 8
+    /// bytes.
+    /// If associated clock is either CLOCK_REALTIME or CLOCK_REALTIME_ALARM,
+    /// timer is absolute (TFD_TIMER_ABSTIME), and flag TFD_TIMER_CANCEL_ON_SET
+    /// was specified when calling timerfd_settime(), then read fails with
+    /// error ECANCELED if real-time clock undergoes discontinuous change.
+    //--------------------------------------------------------------------------
+
+    class HandleReadOnTimerFd : public HandleRead
+    {
+      public:
+
+        HandleReadOnTimerFd() = default;
+
+        void operator()(const ssize_t number_of_bytes)
+        {
+          // TODO: handle EAGAIN, ECANCELED.
+          //get_error_number();
+          //if error_number().error_number()
+
+          HandleRead::operator()(number_of_bytes);
+        }
+    };
+
+    template <
+      int Flags = static_cast<std::underlying_type_t<SetTimeFlags>>(
+        SetTimeFlags::default_value),
+      bool ProvideOldValue = true>
+    void set_time()
+    {
+      int set_time_results;
+
+      // since old_value argument is not NULL (nullptr), then old_value used to
+      // return setting of the timer that was current at the time of the call.
+      if (ProvideOldValue)
+      {
+        set_time_results =
+          ::timerfd_settime(
+          fd_,
+          Flags,
+          new_value_.to_itimerspec_pointer(),
+          old_value_.to_itimerspec_pointer());
+      }
+      else
+      {
+        set_time_results =
+          ::timerfd_settime(
+          fd_,
+          Flags,
+          new_value_.to_itimerspec_pointer(),
+          nullptr);
+      }
+
+      Utilities::ErrorHandling::HandleReturnValue{errno}(
+        set_time_results, "set time (::timerfd_settime)");
+    }
+
     int fd_;
+
+    uint64_t expirations_buffer_;
+
+    IntervalTimerSpecification new_value_;
+    IntervalTimerSpecification old_value_;
+    IntervalTimerSpecification current_value_;
 };
+
+template <ClockIds ClockId, int clock_flags>
+std::ostream& operator<<(std::ostream& os,
+  const TimerFd<ClockId, clock_flags>& tfd)
+{
+  os << "fd : " << tfd.fd_ << '\n' << tfd.new_value() << "old_value: \n" <<
+    tfd.old_value();
+
+  return os;
+}
 
 } // namespace Time
 
--- Cpp14/Time/TimerFd_main.cpp
+++ Cpp14/Time/TimerFd_main.cpp
@@ -25,9 +25,172 @@
 //------------------------------------------------------------------------------
 #include "TimerFd.h"
 
+#include "Specifications.h"
+#include "Utilities/Chrono.h"
+
+#include <iostream>
+#include <thread> // std::this_thread
+#include <type_traits> // std::underlying_type_t
+
+using Time::ClockFlags;
+using Time::IntervalTimerSpecification;
+using Time::SetTimeFlags;
 using Time::TimerFd;
+using Utilities::Milliseconds;
+using Utilities::Seconds;
+
+using namespace Utilities::Literals;
 
 int main()
 {
+  // ClockFlagsHasAllClockFlagsToChangeBehaviorOftimerfd_create
+  {
+    std::cout << "\n ClockFlags \n";
+    std::cout << " ClockFlags::non_blocking : " <<
+      static_cast<int>(ClockFlags::non_blocking) << '\n'; // 0
+    std::cout << " ClockFlags::close_on_execute : " <<
+      static_cast<int>(ClockFlags::close_on_execute) << '\n'; // 1
+  }
+
+  // TimerFdDefaultConstructs
+  {
+    std::cout << "\n TimerFdDefaultConstructs \n";
+    const TimerFd<> tfd;
+    std::cout << tfd << '\n';
+  }
+
+  // TimerFdConstructsWithExplicitInputValues
+  {
+    std::cout << "\n TimerFdConstructsWithExplicitInputValues\n";
+
+    const TimerFd<> tfd {IntervalTimerSpecification{10s, 15s}};
+    std::cout << tfd << '\n';
+
+    const TimerFd<> tfd1 {IntervalTimerSpecification{13ms}};
+    std::cout << tfd1 << '\n';
+  }
+
+  // TimerFdSetTimeWorks
+  {
+    std::cout << "\n TimerFdSetTimeWorks\n";
+    TimerFd<> tfd {IntervalTimerSpecification{10us, 15us}};
+    std::cout << tfd << '\n'; // 0 15000 0 10000 0 0 0 0
+
+    tfd.set_time<
+      static_cast<std::underlying_type_t<SetTimeFlags>>(
+        SetTimeFlags::absolute_time)
+      >(IntervalTimerSpecification{5s, 8s});
+    std::cout << tfd << '\n'; // 8 0 5 0 0 0 0 0
+  }
+
+  // TimerFdGetTimeGetsTimeAfterSetTime
+  {
+    std::cout << "\n TimerFdGetTimeGetsTimeAfterSetTime\n";
+    TimerFd<> tfd {IntervalTimerSpecification{10us, 15us}};
+    tfd.get_time();
+    std::cout << tfd.current_value() << '\n';
+    std::this_thread::sleep_for(2us);
+    tfd.get_time();
+    std::cout << tfd.current_value() << '\n';
+
+    tfd.set_time<
+      static_cast<std::underlying_type_t<SetTimeFlags>>(
+        SetTimeFlags::absolute_time)
+      >(IntervalTimerSpecification{5s, 8s});
+    std::cout << tfd;
+
+    tfd.get_time();
+    std::cout << tfd.current_value() << '\n';
+    std::this_thread::sleep_for(2s);
+    tfd.get_time();
+    std::cout << tfd.current_value() << '\n';
+  }
+
+  // TimerFdGetsTimesCorrectlyBeforeFirstExpiration
+  {
+    std::cout << "\n TimerFdGetsTimesCorrectlyBeforeFirstExpiration \n";
+
+    TimerFd<> tfd {IntervalTimerSpecification{10s}};
+    std::cout << tfd << '\n';
+    tfd.get_time();
+    std::cout << tfd.current_value() << '\n';
+
+    for (int i {1}; i <= 5; ++i)
+    {
+      std::this_thread::sleep_for(2s);
+      tfd.get_time();
+      std::cout << tfd.current_value() << '\n';
+    }
+  }
+
+  // TimerFdReadsAndGivesCorrectExpirations
+  {
+    std::cout << "\n TimerFdReadsAndGivesCorrectExpirations\n";
+    TimerFd<> tfd {IntervalTimerSpecification{5s, 5s}};
+    std::cout << tfd << '\n';
+
+    for (int delta_t {0}; delta_t < 2; ++delta_t)
+    {
+      std::cout << " delta_t : " << delta_t << '\n';
+      std::this_thread::sleep_for(Seconds{delta_t});
+      tfd.read();
+      std::cout << "expirations() : " << tfd.expirations() << '\n';
+    }
+
+    TimerFd<> tfd1 {IntervalTimerSpecification{250ms, 250ms}};
+    std::cout << tfd1 << '\n';
+
+    for (int delta_t {0}; delta_t < 8; ++delta_t)
+    {
+      std::cout << " delta_t : " << delta_t << '\n';
+      std::this_thread::sleep_for(Milliseconds{125 * delta_t});
+      tfd1.read();
+      std::cout << "expirations() : " << tfd1.expirations() << '\n';
+    }
+  }
+
+
+  // TimerFdGetsTimeBeforeExpirationAndAfter
+  {
+    std::cout << "\n TimerFdGetsTimeBeforeExpirationAndAfter\n";
+    TimerFd<> tfd {IntervalTimerSpecification{50ms, 50ms}};
+    std::cout << tfd.get_time();
+
+
+    for (int delta_t {0}; delta_t < 17; ++delta_t)
+    {
+      std::cout << " delta_t : " << delta_t << '\n';
+      std::cout << tfd.get_time() << '\n';
+      std::this_thread::sleep_for(Milliseconds{10 * delta_t});
+      std::cout << tfd.get_time() << '\n';
+      tfd.read();
+      std::cout << "expirations() : " << tfd.expirations() << '\n';
+      std::cout << tfd.get_time() << '\n';
+    }
+  }
+
+
+  // TimerFdCanExpireOnceAndGetsTimeGetsZero
+  {
+    std::cout << "\n TimerFdCanExpireOnceAndGetsTimeGetsZero\n";
+    TimerFd<> tfd {IntervalTimerSpecification{0ms, 250ms}};
+    std::cout << tfd.get_time() << '\n';
+    std::cout << tfd << '\n';
+
+    for (int delta_t {0}; delta_t < 7; ++delta_t)
+    {
+      TimerFd<> tfd {IntervalTimerSpecification{0ms, 50ms}};
+      std::cout << tfd.get_time() << '\n';
+      std::cout << tfd << '\n';
+
+      std::cout << " delta_t : " << delta_t << '\n';
+      std::cout << tfd.get_time() << '\n';
+      std::this_thread::sleep_for(Milliseconds{10 * delta_t});
+      std::cout << tfd.get_time() << '\n';
+      tfd.read();
+      std::cout << "expirations() : " << tfd.expirations() << '\n';
+      std::cout << tfd.get_time() << '\n';
+    }
+  }
 
 }
