cb1cb3c30e638f034f04fa34ccf6e581b8c613aa
ernestyalumni <ernestyalumni@gmail.com>
2019-06-09 15:22:41 -0700

    Working CMakes

---
+++ Voltron/Source/UnitTests/Utilities/FunctionObject_test.cpp
@@ -0,0 +1,19 @@
+// \file FunctionObject_tests.cpp
+
+#include <boost/test/unit_test.hpp>
+#include <iostream>
+
+BOOST_AUTO_TEST_SUITE(Std)
+BOOST_AUTO_TEST_SUITE(FunctionObject_tests)
+
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
+BOOST_AUTO_TEST_CASE(StdFunctionIsFun)
+{
+  std::cout << "\n StdFunctionIsFun\n";
+
+  BOOST_TEST(true);
+}
+
+BOOST_AUTO_TEST_SUITE_END() // FunctionObject_tests
+BOOST_AUTO_TEST_SUITE_END() // Std
