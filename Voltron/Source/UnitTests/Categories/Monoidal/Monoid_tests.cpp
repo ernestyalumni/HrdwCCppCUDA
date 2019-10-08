0484a5931e1a28d24836aa31d9cfbb6ecbd42f07
ernestyalumni <ernestyalumni@gmail.com>
2019-10-08 02:02:50 -0700

    Started Monoid tests

---
+++ Voltron/Source/UnitTests/Categories/Monoidal/Monoid_tests.cpp
@@ -0,0 +1,24 @@
+//------------------------------------------------------------------------------
+// \file Monoid_tests.cpp
+//------------------------------------------------------------------------------
+
+#include <boost/test/unit_test.hpp>
+
+#include <iostream>
+#include <optional>
+
+BOOST_AUTO_TEST_SUITE(Categories)
+BOOST_AUTO_TEST_SUITE(Monoidal)
+BOOST_AUTO_TEST_SUITE(Monoid_tests)
+
+
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
+BOOST_AUTO_TEST_CASE(MonoidExamples)
+{
+  BOOST_TEST(true);
+}
+
+BOOST_AUTO_TEST_SUITE_END() // Monoid_tests
+BOOST_AUTO_TEST_SUITE_END() // Monoidal
+BOOST_AUTO_TEST_SUITE_END() // Categories
