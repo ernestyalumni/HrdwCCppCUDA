ace986b3d77fd4e3b7f69064f73add384410338e
Ernest Yeung <ernestyalumni@gmail.com>
2020-03-23 12:04:23 -0700

    Setup unit tests for BitWidth

--- Voltron/Source/DigitalInts/BitWidth.h
+++ Voltron/Source/DigitalInts/BitWidth.h
@@ -3,10 +3,15 @@
 
 #include <cassert>
 
+namespace DigitalInts
+{
+
 template <std::size_t NBits, class Enable = void>
 class UInt32BitWidth
 {
 	static_assert(NBits <= 32, "Uint32bitwidth must fit in a uint32_t");	
 };
+
+} // namespace DigitalInts
 
 #endif // DIGITAL_INTS_BITWIDTH_H
---
+++ Voltron/Source/UnitTests/DigitalInts/BitWidth_tests.cpp
@@ -0,0 +1,21 @@
+//------------------------------------------------------------------------------
+// \file BitWidth_tests.cpp
+//------------------------------------------------------------------------------
+#include "DigitalInts/BitWidth.h"
+
+#include <boost/test/unit_test.hpp>
+
+using DigitalInts::UInt32BitWidth;
+
+BOOST_AUTO_TEST_SUITE(DigitalInts)
+BOOST_AUTO_TEST_SUITE(BitWidth_tests)
+
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
+BOOST_AUTO_TEST_CASE(NumberOfBitsInAByteIs8)
+{
+}
+
+
+BOOST_AUTO_TEST_SUITE_END() // BitWidth_tests
+BOOST_AUTO_TEST_SUITE_END() // DigitalInts
