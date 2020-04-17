87533ed7c4a4c7f29dbffb2206c4ac4dbd84de39
ernestyalumni <ernestyalumni@gmail.com>
2020-04-17 06:14:39 -0700

    Added units demonstrating explicitly and clearly the relationship between monad, morphisms and code

--- Voltron/Source/Categories/Monads/OptionalMonad.h
+++ Voltron/Source/Categories/Monads/OptionalMonad.h
@@ -17,10 +17,17 @@ namespace Monads
 namespace OptionalMonad
 {
 
+// Note that the endomorphism T : X \to T(X) maps any type T to the type T(X),
+// which in this case is T(X) = std::optional<T>
+
 // In this case for the OptionalMonad, the endofunctor T is std::optional.
 // The type X is an object in the objects for the syntactic category. The
 // objects for this category are the types for this program.
 // What Čukić calls transform is the morphism map for the endofunctor.
+// Indeed, if morphism f : X \to T(Y), then the morphism map for endomorphism T
+// Tf: T(X) \to T(Z) corresponds to this function. Z = T(Y) in this case.
+// \param std::optional<X>& object_element is of type T(X)
+// object_element.value() is of type X.
 template <typename X, typename Morphism>
 auto endomorphism_morphism_map(
   const std::optional<X>& object_element,
@@ -43,12 +50,15 @@ auto endomorphism_morphism_map(
 // In this case for the OptionalMonad, the endofunctor T is std::optional.
 // The type X is an object in the objects for the syntactic category, the
 // types for this program.
+// Indeed, since \mu_X : T^2(X) \to T(X), \mu_X : x \mapsto y, with x \in T^2(X)
+// and y \in T(X).
 template <typename X>
 std::optional<X> multiplication_component(
   const std::optional<std::optional<X>>& object_element)
 {
   if (object_element)
   {
+    // Return an instance of type T(X)
     return object_element.value();
   }
   else
--- Voltron/Source/UnitTests/Categories/Monads/OptionalMonad_tests.cpp
+++ Voltron/Source/UnitTests/Categories/Monads/OptionalMonad_tests.cpp
@@ -28,6 +28,53 @@ std::optional<std::string> to_html(const std::string& text)
   return {};
 }
 
+std::optional<std::string> usable_user_full_name(const std::string& login)
+{
+  if (login == "None")
+  {
+    return {};
+  }
+
+  return std::make_optional<std::string>(login);
+}
+
+std::optional<std::string> usable_to_html(const std::string& text)
+{
+  if (text == "No address")
+  {
+    return {};
+  }
+
+  return std::make_optional<std::string>(text);
+}
+
+// Testfunctions act as morphisms f : X \to T(Y) where T is the endomorphism.
+// T : X \to T(X), where T(X) is of type std::optional
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
+BOOST_AUTO_TEST_CASE(TestFunctionBehavesAsAMorphism)
+{
+  const std::string test_name {"Jean-Francois Roberval"};
+  // result of morphism f of type T(Y), as f : X \to T(Y)
+  const auto result = usable_user_full_name(test_name);
+  BOOST_TEST(static_cast<bool>(result));
+  BOOST_TEST((result.value() == "Jean-Francois Roberval"));
+}
+
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
+BOOST_AUTO_TEST_CASE(EndomorphismMorphismMapWorksOnFunctionReturningStdOptional)
+{
+  const auto test_full_name =
+    std::make_optional<std::string>("Jacques Cartier");
+
+  const auto result =
+    endomorphism_morphism_map(test_full_name, usable_user_full_name);
+
+  BOOST_TEST(static_cast<bool>(result));
+  BOOST_TEST((result.value() == test_full_name.value()));
+}
+
 //------------------------------------------------------------------------------
 //------------------------------------------------------------------------------
 BOOST_AUTO_TEST_CASE(UseMainFromOptionalMonadExampleFromCukic)
