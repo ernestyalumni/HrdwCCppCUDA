104aa91ab5ff7068837cfe27a69cf0299d20c485
ernestyalumni <ernestyalumni@gmail.com>
2020-06-04 04:29:36 -0700

    Added unit tests for Type Utilities by Cukic

--- Voltron/Source/Cpp/Templates/TypeUtilities.h
+++ Voltron/Source/Cpp/Templates/TypeUtilities.h
@@ -4,9 +4,13 @@
 /// \brief 
 /// \ref https://gitlab.com/manning-fpcpp-book/code-examples/blob/master/chapter-11/contained-type/type_utils.h
 ///-----------------------------------------------------------------------------
+#ifndef CPP_TEMPLATES_TYPE_UTILITIES_H
+#define CPP_TEMPLATES_TYPE_UTILITIES_H
 
-#ifndef _CPP_TEMPLATES_TYPE_UTILITIES_H_
-#define _CPP_TEMPLATES_TYPE_UTILITIES_H_
+#include <iterator> // std::begin;
+#include <type_traits> // std::remove_cv_t
+#include <utility> // std::declval
+//#include <vector>
 
 namespace Cpp
 {
@@ -15,12 +19,29 @@ namespace Templates
 namespace TypeUtilities
 {
 
+// cf. https://en.cppreference.com/w/cpp/types/remove_cv
+// template <class T>
+// struct remove_cv
+// provides member typedef type which is same as T, except that its topmost
+// cv-qualifiers are removed.
+// remove_cv - removes topmost const, topmost volatile, or both, if present.
+//
+// cf. https://en.cppreference.com/w/cpp/utility/declval
+// declval - converts any type T to a reference type, making it possible to use
+// member functions in decltype expressions without need to go through ctors.
+//
+// cf. https://en.cppreference.com/w/cpp/types/remove_reference
+// template <class T>
+// struct remove_reference
+// If type T is a reference type, provides member typedef type which is the type
+// referred to by T. Otherwise type is T.
+
 // Meta-function that returns the type of an element in an iterable collection.
 template <typename T>
 using ContainedType =
   std::remove_cv_t<
     std::remove_reference_t<decltype(*std::begin(std::declval<T>()))>
-    >
+    >;
 
 // Meta-function that returns a type with references stripped
 
@@ -29,3 +50,4 @@ using ContainedType =
 } // namespace Templates
 } // namespace Cpp
 
+#endif // CPP_TEMPLATES_TYPE_UTILITIES_H
---
+++ Voltron/Source/UnitTests/Cpp/Templates/TypeUtilities_tests.cpp
@@ -0,0 +1,111 @@
+//------------------------------------------------------------------------------
+/// \file TypeUtilities_tests.cpp
+/// \author Ernest Yeung
+/// \email  ernestyalumni@gmail.com
+/// \ref https://gitlab.com/manning-fpcpp-book/code-examples/-/blob/master/chapter-11/contained-type/type_utils.h
+//------------------------------------------------------------------------------
+#include "Cpp/Templates/TypeUtilities.h"
+
+#include <boost/test/unit_test.hpp>
+#include <string>
+#include <type_traits> // std::is_same, std::remove_reference
+#include <utility> // std::declval
+#include <vector>
+
+using Cpp::Templates::TypeUtilities::ContainedType;
+
+BOOST_AUTO_TEST_SUITE(Cpp)
+BOOST_AUTO_TEST_SUITE(Templates)
+BOOST_AUTO_TEST_SUITE(TypeUtilities_tests)
+
+struct Default
+{
+  int foo() const
+  {
+    return 1;
+  }
+};
+
+struct NonDefault
+{
+  NonDefault() = delete;
+  int foo() const
+  {
+    return 1;
+  }
+};
+
+// cf. https://en.cppreference.com/w/cpp/utility/declval
+// std::declval
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
+BOOST_AUTO_TEST_CASE(StdDeclvalConvertsAnyTypeWithoutDefaultConstructor)
+{
+  decltype(Default().foo()) n1 {1}; // type of n1 is int
+  // decltype(NonDefault().foo()) n2 = n1; // error: no default constructor
+  decltype(std::declval<NonDefault>().foo()) n2 {n1}; // type of n2 is int
+  BOOST_TEST(n1 == 1);
+  BOOST_TEST(n2 == 1);
+}
+
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
+BOOST_AUTO_TEST_CASE(StdRemoveReferenceProvidesTypeReferredTo)
+{
+  BOOST_TEST((std::is_same<int, int>()));
+  BOOST_TEST(!(std::is_same<int, int&>()));
+  BOOST_TEST(!(std::is_same<int, int&&>()));
+
+  BOOST_TEST((std::is_same<int, std::remove_reference_t<int>>()));
+  BOOST_TEST((std::is_same<int, std::remove_reference_t<int&>>()));
+  BOOST_TEST((std::is_same<int, std::remove_reference_t<int&&>>()));
+}
+
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
+BOOST_AUTO_TEST_CASE(StdRemoveCvRemovesConstVolatileQualifiersFromTypes)
+{
+  typedef std::remove_cv_t<const int> type1;
+  typedef std::remove_cv_t<volatile int> type2;
+  typedef std::remove_cv_t<const volatile int> type3;
+
+  BOOST_TEST((std::is_same<int, type1>()));
+  BOOST_TEST((std::is_same<int, type2>()));
+  BOOST_TEST((std::is_same<int, type3>()));
+}
+
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
+BOOST_AUTO_TEST_CASE(StdRemoveCvRemovesConstVolatileQualifiersFromPointerTypes)
+{
+  typedef std::remove_cv_t<const volatile int*> type4;
+  typedef std::remove_cv_t<int* const volatile> type5;
+
+  BOOST_TEST((std::is_same<const volatile int*, type4>()));
+  BOOST_TEST(!(std::is_same<int*, type4>()));
+  BOOST_TEST((std::is_same<int*, type5>()));
+}
+
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
+BOOST_AUTO_TEST_CASE(ContainedTypeGetsTypeOfContainerElements)
+{
+  BOOST_TEST((
+    std::is_same<
+      ContainedType<std::vector<std::string>>,
+      std::string
+      >()
+    ));
+
+  BOOST_TEST((
+    std::is_same<
+      NonDefault,
+      ContainedType<std::vector<NonDefault>>
+      >()
+    ));
+}
+
+BOOST_AUTO_TEST_SUITE_END() // TypeUtilities_tests 
+
+BOOST_AUTO_TEST_SUITE_END() // Templates
+BOOST_AUTO_TEST_SUITE_END() // Cpp
