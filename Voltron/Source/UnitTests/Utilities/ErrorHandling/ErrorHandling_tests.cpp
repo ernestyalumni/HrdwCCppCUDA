fa8730ede61149c645d87c294bdc10e3f309f8ae
Ernest Yeung <ernestyalumni@gmail.com>
2020-06-07 00:22:50 -0700

    Add ErrorHandling tests

---
+++ Voltron/Source/UnitTests/Utilities/ErrorHandling/ErrorHandling_tests.cpp
@@ -0,0 +1,20 @@
+//------------------------------------------------------------------------------
+// \file ErrorHandling_tests.cpp
+//------------------------------------------------------------------------------
+#include "Utilities/ErrorHandling/ErrorHandling.h"
+
+#include <boost/test/unit_test.hpp>
+
+
+BOOST_AUTO_TEST_SUITE(Utilities)
+BOOST_AUTO_TEST_SUITE(ErrorHandling_tests)
+
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
+BOOST_AUTO_TEST_CASE(PrintsRValueCharAsHex)
+{
+	BOOST_TEST(false);
+}
+
+BOOST_AUTO_TEST_SUITE_END() // ErrorHandling_tests
+BOOST_AUTO_TEST_SUITE_END() // Utilities
