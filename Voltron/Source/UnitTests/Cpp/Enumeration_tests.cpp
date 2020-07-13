8cff4f31c49d968e696a844c4cdbd6bd06893a41
ernestyalumni <ernestyalumni@gmail.com>
2020-07-13 13:24:50 -0700

    Another unit test for enum classes

--- Voltron/Source/UnitTests/Cpp/Enumeration_tests.cpp
+++ Voltron/Source/UnitTests/Cpp/Enumeration_tests.cpp
@@ -1,10 +1,15 @@
 //------------------------------------------------------------------------------
 // \file Enumeration_tests.cpp
 //------------------------------------------------------------------------------
+#include "Cpp/Numerics/BitCast.h"
+#include "Cpp/Utilities/SuperBitSet.h"
 #include "Cpp/Utilities/TypeSupport/UnderlyingTypes.h"
 
 #include <boost/test/unit_test.hpp>
+#include <utility>
 
+using Cpp::Numerics::bit_cast;
+using Cpp::Utilities::SuperBitSet;
 using Cpp::Utilities::TypeSupport::get_underlying_value;
 
 BOOST_AUTO_TEST_SUITE(Cpp)
@@ -68,7 +73,61 @@ BOOST_AUTO_TEST_CASE(UnsignedCharValuesDefaultToNumbers)
 	BOOST_TEST((get_underlying_value(DefaultUnsignedChars::Deux) == message2[2]));
 	BOOST_TEST(
 		(get_underlying_value(DefaultUnsignedChars::Trois) == message2[3]));
+}
 
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
+BOOST_AUTO_TEST_CASE(EnumClassValuesCanBeComparedWithBitwiseOperations)
+{
+	BOOST_TEST(
+		get_underlying_value(DefaultUnsignedChars::Trois) ==
+			get_underlying_value(DefaultUnsignedChars::Un) |
+				get_underlying_value(DefaultUnsignedChars::Deux));
+
+	SuperBitSet<8> un_bits8 {bit_cast<uint8_t>(DefaultUnsignedChars::Un)};
+	BOOST_TEST(un_bits8.to_string() == "00000001");
+
+	SuperBitSet<8> deux_bits8 {bit_cast<uint8_t>(DefaultUnsignedChars::Deux)};
+	BOOST_TEST(deux_bits8.to_string() == "00000010");
+
+	SuperBitSet<8> trois_bits8 {bit_cast<uint8_t>(DefaultUnsignedChars::Trois)};
+	BOOST_TEST(trois_bits8.to_string() == "00000011");
+
+	const int32_t un_et_trois {
+		get_underlying_value(DefaultUnsignedChars::Un) &
+			get_underlying_value(DefaultUnsignedChars::Trois)};
+
+	SuperBitSet<32> un_et_trois_bits32 {bit_cast<uint32_t>(un_et_trois)};
+	BOOST_TEST(un_et_trois_bits32.to_string() ==
+		"00000000000000000000000000000001");
+	
+	/* // Narrowing conversion
+	const bool est_un_et_trois {
+		get_underlying_value(DefaultUnsignedChars::Un) &
+			get_underlying_value(DefaultUnsignedChars::Trois)};
+	*/
+
+	BOOST_TEST(static_cast<bool>(un_et_trois));
+
+	const int32_t deux_et_trois {
+		get_underlying_value(DefaultUnsignedChars::Deux) &
+			get_underlying_value(DefaultUnsignedChars::Trois)};
+
+	SuperBitSet<32> deux_et_trois_bits32 {bit_cast<uint32_t>(deux_et_trois)};
+	BOOST_TEST(deux_et_trois_bits32.to_string() ==
+		"00000000000000000000000000000010");
+
+	BOOST_TEST(static_cast<bool>(deux_et_trois));
+
+	const int32_t zero_et_trois {
+		get_underlying_value(DefaultUnsignedChars::Zero) &
+			get_underlying_value(DefaultUnsignedChars::Trois)};
+	BOOST_TEST(!static_cast<bool>(zero_et_trois));
+
+	const int32_t quatre_et_trois {
+		get_underlying_value(DefaultUnsignedChars::Quatre) &
+			get_underlying_value(DefaultUnsignedChars::Trois)};
+	BOOST_TEST(!static_cast<bool>(quatre_et_trois));
 }
 
 BOOST_AUTO_TEST_SUITE_END() // Enumeration_tests
