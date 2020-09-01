005426ce17709d5a689b146ca6bd36025dd15b1c
ernestyalumni <ernestyalumni@gmail.com>
2020-09-01 05:02:09 -0700

    Unit tests for streams begins

---
+++ Voltron/Source/UnitTests/Cpp/IO/Streams_tests.cpp
@@ -0,0 +1,22 @@
+//------------------------------------------------------------------------------
+/// \file Streams_tests.cpp
+//------------------------------------------------------------------------------
+#include "Tools/TemporaryDirectory.h"
+
+#include <boost/test/unit_test.hpp>
+
+BOOST_AUTO_TEST_SUITE(Cpp)
+BOOST_AUTO_TEST_SUITE(IO)
+BOOST_AUTO_TEST_SUITE(Streams_tests)
+
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
+BOOST_AUTO_TEST_CASE(IfstreamExamples)
+{
+  BOOST_TEST(true);
+}
+
+BOOST_AUTO_TEST_SUITE_END() // Streams_tests 
+
+BOOST_AUTO_TEST_SUITE_END() // IO
+BOOST_AUTO_TEST_SUITE_END() // Cpp
