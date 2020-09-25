0e44613c10dba1f73c66b2fe902f2fe27fe88287
ernestyalumni <ernestyalumni@gmail.com>
2020-09-25 09:32:56 -0700

    Notes on computer data storage, and example

---
+++ Voltron/Source/UnitTests/Systems/DisplayMemoryAddress.cpp
@@ -0,0 +1,16 @@
+//------------------------------------------------------------------------------
+/// \file DisplayMemoryAddress
+/// \ref https://www.tenouk.com/cpluscodesnippet/cplusarraypointermemoryaddress.html
+/// \brief A program that uses pointers to print the array elements
+//------------------------------------------------------------------------------
+#include <iostream>
+
+int main()
+{
+  // declare and initialize an array nums.
+  int nums[] = {92, 81, 70, 69, 58};
+
+  std::cout << "\nArray's element Memory address";
+
+  return 0;
+}
