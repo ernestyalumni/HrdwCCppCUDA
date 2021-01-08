dc310617eaeb5955afc7ffcc648535de208c3fd8
Ernest Yeung <ernestyalumni@gmail.com>
2021-01-08 02:43:29 -0800

    Updated for Problem Set 1 MIT 6.0001

--- Voltron/Voltron/MIT_6_0001/Fall2016_ps.py
+++ Voltron/Voltron/MIT_6_0001/Fall2016_ps.py
@@ -28,3 +28,340 @@ def very_simple_program():
 	print("log(x) = " + str(log_result))
 
 	return (power_result, log_result)
+
+"""
+Problem Set 1
+
+@details Introduce you to using control flow in Python and formulating a
+computational solution to a problem.
+"""
+
+class HouseSavingsAndInvestmentValues:
+	# Portion of cost needed for down payment.
+	# For simplicity, assume it's 0.25 (25%)
+	portion_down_payment = 0.25
+
+	# Annual return due to investment of your current savings.
+	# Assume your investments return r = 0.04 (4%)
+	r = 0.04
+
+
+class HouseSavings:
+
+	@staticmethod
+	def monthly_return(current_savings, r):
+		"""
+		@brief Returns the additional funds to put into savings.
+		"""
+		return current_savings * r / 12
+
+	@staticmethod
+	def monthly_salary(annual_salary):
+		"""
+		@fn monthly_salary
+
+		@details At end of each month, your savings will be increased by the
+		return on your investment, plus percentage of your monthly salary
+		(annual salary / 12)
+		"""
+		return annual_salary / 12
+
+	def __init__(self):
+		# Amount you've saved thus far. Start with 0.
+		self.current_savings = 0
+
+	def calculate_months_for_down_payment(self,
+			annual_salary,
+			portion_saved,
+			# Cost of home.
+			total_cost):
+		portion_down = HouseSavingsAndInvestmentValues.portion_down_payment
+
+		down_payment_needed = total_cost * portion_down
+
+		months = 0
+		while (down_payment_needed > self.current_savings):
+			monthly_savings = self.calculate_monthly_savings(
+				annual_salary,
+				portion_saved)
+
+			self.current_savings += monthly_savings
+			months += 1
+
+		return months
+
+	def calculate_monthly_savings(
+			self,
+			annual_salary,
+			portion_saved,
+			return_rate = HouseSavingsAndInvestmentValues.r):
+		"""
+		@details This is a 2 part calculation.
+		"""
+
+		# Calculate savings from salary.
+
+		monthly_salary_amount = HouseSavings.monthly_salary(annual_salary)
+		# At the end of each month, savings increased by a percentage of your
+		# monthly salary, portion_saved.
+		monthly_salary_saved = monthly_salary_amount * portion_saved
+
+		# Calculate return on investing savings.
+
+		#return_rate = HouseSavingsAndInvestmentValues.r
+		return_on_investment = HouseSavings.monthly_return(
+			self.current_savings,
+			return_rate)
+
+		# At the end of each moth, savings increased by return on investment,
+		# plus percentage of monthly salary
+		return return_on_investment + monthly_salary_saved
+
+
+def get_calculate_months_for_down_payment_inputs():
+	annual_salary = float(input("Enter your annual salary: "))
+	portion_saved = float(input(
+		"Enter the percent of your salary to save, as a decimal: "))
+	total_cost = float(input("Enter the cost of your dream home: "))
+
+	return (annual_salary, portion_saved, total_cost)	
+
+
+def calculate_months_for_down_payment():
+
+	annual_salary, portion_saved, total_cost = \
+		get_calculate_months_for_down_payment_inputs()
+
+	house_savings = HouseSavings()
+	months_needed = house_savings.calculate_months_for_down_payment(
+		annual_salary,
+		portion_saved,
+		total_cost)
+
+	print("Number of months: ", months_needed)
+	return months_needed
+
+
+"""
+Problem Set 1
+Part B: Saving, with a raise
+"""
+
+class HouseSavingsWithRaise:
+
+	def __init__(self):
+		self.house_savings = HouseSavings()
+
+
+	def calculate_months_for_down_payment(self,
+			annual_salary,
+			portion_saved,
+			# Cost of home.
+			total_cost,
+			semi_annual_raise):
+		portion_down = HouseSavingsAndInvestmentValues.portion_down_payment
+
+		down_payment_needed = total_cost * portion_down
+
+		months = 0
+		current_annual_salary = annual_salary
+
+		while (down_payment_needed > self.house_savings.current_savings):
+
+			# After the 6th month and after 12th month, 18th month, and so on.
+			if (months > 0 and (months % 6 == 0)):
+				current_annual_salary += (current_annual_salary
+					* semi_annual_raise)
+
+			# Reuse the calculation method from HouseSavings.
+			monthly_savings = self.house_savings.calculate_monthly_savings(
+				current_annual_salary,
+				portion_saved)
+
+			self.house_savings.current_savings += monthly_savings
+			months += 1
+
+		return months
+
+
+def get_down_payment_time_with_raise_inputs():
+	annual_salary, portion_saved, total_cost = \
+		get_calculate_months_for_down_payment_inputs()
+
+	# Decimal percentage raise every 6 months.
+	semi_annual_raise = float(input(
+		"Enter the percent raise every 6 months, as a decimal: "))
+
+	return (annual_salary, portion_saved, total_cost, semi_annual_raise)
+
+
+def down_payment_time_with_raise():
+
+	annual_salary, portion_saved, total_cost, semi_annual_raise = \
+		get_down_payment_time_with_raise_inputs()
+
+
+	house_savings = HouseSavingsWithRaise()
+	months_needed = house_savings.calculate_months_for_down_payment(
+		annual_salary,
+		portion_saved,
+		total_cost,
+		semi_annual_raise)
+
+	print("Number of months: ", months_needed)
+	return months_needed
+
+
+"""
+Problem Set 1
+Part C: Finding the right amount to save away
+"""
+
+class MonthlySavingsGoalValues:
+	# Semi-annual raise in decimal
+	semi_annual_raise = .07
+	# investment annual return
+	r = 0.04
+	# Down payment percent portion of cost of house.
+	portion_down_payment = 0.25
+	# Cost of the house that you're saving for
+	total_house_cost = 1000000
+
+	# How much to save?
+	initial_portion_saved_lower_bound = 0
+	initial_portion_saved_upper_bound = 10000
+
+	# In months
+	time_goal = 36
+
+	# Within $100 or less
+	upper_bound_error = 100
+
+
+class MonthlySavingsGoal:
+
+	def __init__(self):
+		self.house_savings = HouseSavings()
+		self.compute_down_payment()
+
+	def compute_down_payment(self):
+
+		self.down_payment_goal = (MonthlySavingsGoalValues.total_house_cost *
+			MonthlySavingsGoalValues.portion_down_payment)
+
+	def reset_current_savings(self):
+		self.house_savings.current_savings = 0
+
+	def compute_current_savings(self, annual_salary, portion_saved):
+
+		semi_annual_raise = MonthlySavingsGoalValues.semi_annual_raise
+
+		months = 0
+		current_annual_salary = annual_salary
+
+		while (months < MonthlySavingsGoalValues.time_goal):
+
+			# After the 6th month and after 12th month, 18th month, and so on.
+			if (months > 0 and (months % 6 == 0)):
+				current_annual_salary += (current_annual_salary
+					* semi_annual_raise)
+
+			# Reuse the calculation method from HouseSavings.
+			monthly_savings = self.house_savings.calculate_monthly_savings(
+				current_annual_salary,
+				portion_saved,
+				MonthlySavingsGoalValues.r)
+
+			self.house_savings.current_savings += monthly_savings
+			months += 1
+
+		current_savings = self.house_savings.current_savings
+		self.reset_current_savings()
+
+		return current_savings
+
+
+	def initial_lower_bound_check(self, annual_salary):
+		"""
+		Returns false if down payment goal can be met without saving any salary
+		"""
+		portion_saved = \
+			MonthlySavingsGoalValues.initial_portion_saved_lower_bound / 10000
+
+		current_savings = self.compute_current_savings(
+			annual_salary,
+			portion_saved)
+
+		return current_savings < self.down_payment_goal
+
+
+	def initial_upper_bound_check(self, annual_salary):
+		"""
+		Returns false if down payment goal cannot be met at all.
+		"""
+		portion_saved = \
+			MonthlySavingsGoalValues.initial_portion_saved_upper_bound / 10000
+
+		current_savings = self.compute_current_savings(
+			annual_salary,
+			portion_saved)
+
+		return current_savings >= self.down_payment_goal
+
+
+	def iterative_bisection(self, lower_bound, upper_bound, annual_salary):
+		if (lower_bound == upper_bound):
+			return (lower_bound, upper_bound)
+
+		average_in_decimal = (lower_bound + upper_bound) / 2.0 / 10000
+		average_in_int = int((lower_bound + upper_bound) / 2)
+
+		# Handle the case when they are only value 1 apart. Remember that int
+		# casting truncates - it doesn't round.
+		if (average_in_int == lower_bound):
+			return (upper_bound, upper_bound)
+
+		current_savings = self.compute_current_savings(
+			annual_salary,
+			average_in_decimal)
+
+		if (current_savings >= self.down_payment_goal and
+			(current_savings - self.down_payment_goal <
+				MonthlySavingsGoalValues.upper_bound_error)):
+			return (average_in_int, average_in_int)
+
+		if (current_savings > self.down_payment_goal):
+			return (lower_bound, average_in_int)
+		# otherwise it must be that
+		# current_savings < self.down_payment_goal
+		else:
+			return (average_in_int, upper_bound)
+
+
+	def iterative_bisection_search(self, annual_salary):
+		# Initialize lower and upper bounds.
+		lower_bound = \
+			MonthlySavingsGoalValues.initial_portion_saved_lower_bound		
+
+		upper_bound = \
+			MonthlySavingsGoalValues.initial_portion_saved_upper_bound
+
+		if not self.initial_upper_bound_check(annual_salary):
+			print("It is not possible to pay the down payment in three years")
+			return None
+
+		iterative_steps = 0
+
+		while (lower_bound < upper_bound):
+			lower_bound, upper_bound = self.iterative_bisection(
+				lower_bound,
+				upper_bound,
+				annual_salary)
+
+			iterative_steps += 1
+
+			# Should terminate here.
+			if (lower_bound == upper_bound):
+				return (lower_bound, iterative_steps)
+
+		return ((lower_bound, upper_bound), iterative_steps)
--- Voltron/unit_tests/test_MIT_OCW_6_0001_Fall2016_ps.py
+++ Voltron/unit_tests/test_MIT_OCW_6_0001_Fall2016_ps.py
@@ -13,7 +13,11 @@ pytest test_MIT_OCW_6_0001_Fall2016_ps.py
 
 """
 from Voltron.MIT_6_0001 import Fall2016_ps
-from Voltron.MIT_6_0001.Fall2016_ps import (very_simple_program)
+from Voltron.MIT_6_0001.Fall2016_ps import (
+	calculate_months_for_down_payment,
+	down_payment_time_with_raise,
+	very_simple_program,
+	MonthlySavingsGoal)
 
 from unittest import mock
 
@@ -22,7 +26,7 @@ import pytest
 # Don't include the prefix; otherwise get error message: "Fixtures are not
 # meant to be called directly."
 #@pytest.fixture
-def test_values():
+def very_simple_program_test_values():
 
 	class TestValues:
 		x = 2
@@ -31,18 +35,18 @@ def test_values():
 	return TestValues()
 
 
-def mock_input(input_prompt):
+def mock_very_simple_program_input(input_prompt):
 	"""
 	@url https://stackoverflow.com/questions/35851323/how-to-test-a-function-with-input-call
 	@url https://stackoverflow.com/questions/32682428/python-using-mock-for-a-multiple-user-inputs 
 	"""
 	if "x" in input_prompt.lower():
-		return test_values().x
+		return very_simple_program_test_values().x
 	if "y" in input_prompt.lower():
-		return test_values().y
+		return very_simple_program_test_values().y
 
 
-@mock.patch('builtins.input', mock_input)
+@mock.patch('builtins.input', mock_very_simple_program_input)
 def test_very_simple_program():
 	"""
 	@details
@@ -55,3 +59,218 @@ def test_very_simple_program():
 	"""
 	#with mock.patch.object(__builtins__, 'input', mock_input):
 	assert very_simple_program() == (8, 1.0)
+
+
+"""
+Problem Set 1
+MIT 6.0001 Fall 2016
+"""
+
+
+def calculate_months_for_down_payment_test_values(option = 1):
+
+	class TestValues1:
+		annual_salary = 120000
+		portion_saved = .10
+		total_cost = 1000000
+
+	class TestValues2:
+		annual_salary = 80000
+		portion_saved = .15
+		total_cost = 500000
+
+	if option == 1:
+		return TestValues1()
+	else:
+		return TestValues2()
+
+
+def mock_calculate_months_for_down_payment_closure(option = 1):
+	"""
+	@fn mock_calculate_months_for_down_payment_closure
+	"""
+
+	def mock_calculate_months_for_down_payment_input(input_prompt):
+		if "annual salary" in input_prompt.lower():
+			return calculate_months_for_down_payment_test_values(
+				option).annual_salary
+		if "percent of your salary to save" in input_prompt.lower():
+			return calculate_months_for_down_payment_test_values(
+				option).portion_saved
+		if "cost of your dream home" in input_prompt.lower():
+			return calculate_months_for_down_payment_test_values(
+				option).total_cost
+
+	return mock_calculate_months_for_down_payment_input
+
+
+@mock.patch(
+	'builtins.input',
+	mock_calculate_months_for_down_payment_closure(1))
+def test_calculate_months_for_down_payment_test_case_1():
+	result = calculate_months_for_down_payment()
+	assert result == 183
+
+@mock.patch(
+	'builtins.input',
+	mock_calculate_months_for_down_payment_closure(2))
+def test_calculate_months_for_down_payment_test_case_2():
+	result = calculate_months_for_down_payment()
+	assert result == 105
+
+
+"""
+Problem Set 1: Part B
+"""
+
+def down_payment_time_with_raise_test_values(option = 1):
+
+	class TestValues1:
+		annual_salary = 120000
+		portion_saved = .05
+		total_cost = 500000
+		semi_annual_raise = .03
+
+	class TestValues2:
+		annual_salary = 80000
+		portion_saved = .1
+		total_cost = 800000
+		semi_annual_raise = .03
+
+	class TestValues3:
+		annual_salary = 75000
+		portion_saved = .05
+		total_cost = 1500000
+		semi_annual_raise = .05
+
+
+	if option == 1:
+		return TestValues1()
+	elif option == 2:
+		return TestValues2()
+	else:
+		return TestValues3()
+
+
+def mock_down_payment_time_with_raise_closure(option = 1):
+	"""
+	@fn mock_down_payment_time_with_raise_closure
+	"""
+
+	def mock_down_payment_time_with_raise_input(input_prompt):
+		if "annual salary" in input_prompt.lower():
+			return down_payment_time_with_raise_test_values(
+				option).annual_salary
+		if "percent of your salary to save" in input_prompt.lower():
+			return down_payment_time_with_raise_test_values(
+				option).portion_saved
+		if "cost of your dream home" in input_prompt.lower():
+			return down_payment_time_with_raise_test_values(
+				option).total_cost
+#		if "semi-annual salary raise" in input_prompt.lower():
+		if "percent raise every 6 months" in input_prompt.lower():
+			return down_payment_time_with_raise_test_values(
+				option).semi_annual_raise
+
+		# Should not reach here - should be an error.
+		return None
+
+	return mock_down_payment_time_with_raise_input
+
+
+@mock.patch(
+	'builtins.input',
+	mock_down_payment_time_with_raise_closure(1))
+def test_down_payment_time_with_raise_test_case_1():
+	result = down_payment_time_with_raise()
+	assert result == 142
+
+@mock.patch(
+	'builtins.input',
+	mock_down_payment_time_with_raise_closure(2))
+def test_down_payment_time_with_raise_test_case_2():
+	result = down_payment_time_with_raise()
+	assert result == 159
+
+@mock.patch(
+	'builtins.input',
+	mock_down_payment_time_with_raise_closure(3))
+def test_down_payment_time_with_raise_test_case_3():
+	result = down_payment_time_with_raise()
+	assert result == 261
+
+
+"""
+Problem Set 1
+Part C: Finding the right amount to save away
+
+Try different inputs for your starting salary, and see how percentage you
+need to save changes to reach desired down payment.
+"""
+
+def savings_rate_test_values(option = 1):
+
+	class TestValues1:
+		starting_annual_salary = 150000
+		expected_savings_rate = 0.4411
+		expected_bisection_steps = 12
+
+	class TestValues2:
+		starting_annual_salary = 300000
+		expected_savings_rate = 0.2206
+		expected_bisection_steps = 9
+
+	class TestValues3:
+		starting_annual_salary = 10000
+
+	if option == 1:
+		return TestValues1()
+	elif option == 2:
+		return TestValues2()
+	else:
+		return TestValues3()
+
+def mock_savings_rate_closure(option = 1):
+	"""
+	@fn mock_savings_rate_closure
+	"""
+	def mock_savings_rate_input(input_prompt):
+		return savings_rate_test_values(option).starting_annual_salary
+
+	return mock_savings_rate_input
+
+def test_MonthlySavingsGoal_test_case_1():
+	starting_annual_salary = savings_rate_test_values(1).starting_annual_salary
+	expected_savings_rate = savings_rate_test_values(1).expected_savings_rate
+	expected_bisection_steps = savings_rate_test_values(
+		1).expected_bisection_steps
+
+	g = MonthlySavingsGoal()
+	savings_rate, steps = g.iterative_bisection_search(starting_annual_salary)
+
+	assert ((savings_rate / 10000) == expected_savings_rate and 
+		steps == expected_bisection_steps)
+
+
+def test_MonthlySavingsGoal_test_case_2():
+	starting_annual_salary = savings_rate_test_values(2).starting_annual_salary
+	expected_savings_rate = savings_rate_test_values(2).expected_savings_rate
+
+	# My iteration steps don't match what's given in MIT OCW 6.0001 F16.
+	expected_bisection_steps = savings_rate_test_values(
+		2).expected_bisection_steps
+
+	g = MonthlySavingsGoal()
+	savings_rate, steps = g.iterative_bisection_search(starting_annual_salary)
+
+	assert ((savings_rate / 10000) == expected_savings_rate and 
+		steps == 15)
+
+
+def test_MonthlySavingsGoal_test_case_3():
+	starting_annual_salary = savings_rate_test_values(3).starting_annual_salary
+
+	g = MonthlySavingsGoal()
+	result = g.iterative_bisection_search(starting_annual_salary)
+
+	assert result == None
