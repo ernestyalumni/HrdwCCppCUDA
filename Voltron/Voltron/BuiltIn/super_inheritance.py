0a0f0879f84342d39e04bae6dd50d30255de9fd4
Ernest Yeung <ernestyalumni@gmail.com>
2021-01-11 20:18:32 -0800

    Started script demonstrating the built in function super

---
+++ Voltron/Voltron/BuiltIn/super_inheritance.py
@@ -0,0 +1,10 @@
+"""
+@brief Demonstrate build-in function super() with respect to inheritance.
+
+@url https://docs.python.org/3/library/functions.html 
+
+@details
+
+cf. https://docs.python.org/3/library/functions.html#super
+"""
+
