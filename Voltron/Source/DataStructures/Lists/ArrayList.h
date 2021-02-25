36b9521110a0cfc5801344c643c5fbd5444032f7
ernestyalumni <ernestyalumni@gmail.com>
2021-02-24 23:26:37 -0800

    Added to ArrayList and List from Shaffer

--- Voltron/Source/DataStructures/Lists/ArrayList.h
+++ Voltron/Source/DataStructures/Lists/ArrayList.h
@@ -4,11 +4,14 @@
 /// \brief Classes and functions demonstrating list as an Abstract Data Type.
 /// \ref Data Structures and Algorithm Analysis in C++, 3rd. Ed.. Dr. Clifford
 /// A. Shaffer. Fig. 4.2. An array-based list implementation.
+/// \ref https://github.com/OpenDSA/OpenDSA/blob/master/SourceCode/C%2B%2B_Templates/Lists/AList.h
 //-----------------------------------------------------------------------------
 #ifndef DATA_STRUCTURES_LISTS_ARRAY_LIST_H
 #define DATA_STRUCTURES_LISTS_ARRAY_LIST_H
 
 #include "List.h"
+
+#include <stdexcept> // std::out_of_range
 
 namespace DataStructures
 {
@@ -18,25 +21,203 @@ namespace Lists
 //-----------------------------------------------------------------------------
 /// \brief Array-based list implementation
 //-----------------------------------------------------------------------------
-template <typename E>
-class ArrayList : public List<E>
+template <typename T>
+class ArrayList : public List<T>
 {
 	public:
 
+		// Default size.
+		static constexpr int default_size {10};
+
+		// Constructors
+
+    //--------------------------------------------------------------------------
+    /// \brief Create a new list object with maximum size "size".
+    //--------------------------------------------------------------------------
 		ArrayList(const int size = default_size) :
 			max_size_{size},
 			list_size_{0},
 			current_{0},
-			list_array_{new E[max_size_]}
-		{
-		}
+			list_array_{new T[max_size_]}
+		{}
 
     //--------------------------------------------------------------------------
-    /// \brief Destructor.
+    /// \brief Destructor. Destructor to remove array.
     //--------------------------------------------------------------------------
 		~ArrayList()
 		{
 			delete [] list_array_;
+		}
+
+    //--------------------------------------------------------------------------
+    /// \brief Reinitialize the list.
+    //--------------------------------------------------------------------------
+		void clear()
+		{
+			// Remove the array.
+			delete [] list_array_;
+
+			// Reset the size.
+			list_size_ = current_ = 0;
+
+
+			// Recreate array.
+			list_array_ = new T[max_size_];
+		}
+
+    //--------------------------------------------------------------------------
+    /// \brief Insert "it" at current position.
+    //--------------------------------------------------------------------------
+		void insert(const T& it)
+		{
+
+			//assert(list_size_ < max_size_, "List capacity exceeded"); // Original
+
+			if (list_size_ >= max_size_)
+			{
+				return;
+			}
+
+			// Shift elements up to make room
+			//
+			// Time complexity: worst case O(N).
+			for (int i {list_size_}; i > current_; --i)
+			{
+				list_array_[i] = list_array_[i - 1];
+			}
+
+			list_array_[current_] = it;
+
+			// Increment list size.
+			++list_size_;
+		}
+
+    //--------------------------------------------------------------------------
+    /// \brief Append "it".
+    //--------------------------------------------------------------------------
+		void append(const T& it)
+		{
+			//assert(list_size_ < max_size_, "List capacity exceeded"); // Original
+			if (list_size_ >= max_size_)
+			{
+				return;
+			}
+
+			list_array_[list_size_++] = it;
+		}
+
+    //--------------------------------------------------------------------------
+    /// \brief Remove and return the current element.
+    //--------------------------------------------------------------------------
+		T remove()
+		{
+			// originally,
+			//assert((current_ >= 0) && (current_ < list_size_), "No element");
+
+			if (current_ < 0 || current_ >= list_size_)
+			{
+				throw std::out_of_range("remove() in ArrayList has current of");
+			}
+
+			// Copy the element.
+			T it {list_array_[current_]};
+
+			// Shift them down.
+			for (int i {current_}; i < list_size_ - 1; ++i)
+			{
+				list_array_[i] = list_array_[i + 1];
+			}
+
+			// Decrement size.
+			list_size_--;
+
+			return it;
+		}
+
+    //--------------------------------------------------------------------------
+    /// \brief Reset position.
+    //--------------------------------------------------------------------------
+		void move_to_start()
+		{
+			current_ = 0;
+		}
+
+    //--------------------------------------------------------------------------
+    /// \brief Set at end.
+    //--------------------------------------------------------------------------
+		void move_to_end()
+		{
+			current_ = list_size_;
+		}
+
+    //--------------------------------------------------------------------------
+    /// \brief Back up.
+    //--------------------------------------------------------------------------
+		void previous()
+		{
+			if (current_ != 0)
+			{
+				current_--;
+			}
+		}
+
+    //--------------------------------------------------------------------------
+    /// \brief Next.
+    //--------------------------------------------------------------------------
+		void next()
+		{
+			if (current_ < list_size_)
+			{
+				current_++;
+			}
+		}
+
+    //--------------------------------------------------------------------------
+    /// \brief Return list size.
+    //--------------------------------------------------------------------------
+		int length() const
+		{
+			return list_size_;
+		}
+
+    //--------------------------------------------------------------------------
+    /// \brief Return current position.
+    //--------------------------------------------------------------------------
+		int current_position() const
+		{
+			return current_;
+		}
+
+    //--------------------------------------------------------------------------
+    /// \brief Set current list position to "position".
+    //--------------------------------------------------------------------------
+		void move_to_position(int position)
+		{
+			//assert(
+			//	(position >= 0) && (position <= list_size_),
+			//	"Position out of range"); // Original
+
+			if (position < 0 || position > list_size_)
+			{
+				return;
+			}
+
+			current_ = position;
+		}
+
+    //--------------------------------------------------------------------------
+    /// \brief Return current element
+    //--------------------------------------------------------------------------
+		const T& get_value() const
+		{
+			// Originally,
+			//assert((current_ >= 0) && (current_ < list_size_), "No current element");
+			if (current_ < 0 || current_ >= list_size_)
+			{
+				throw std::out_of_range("get_value() in ArrayList has current of ");
+			}
+
+			return list_array_[current_];			
 		}
 
 	private:
@@ -51,8 +232,7 @@ class ArrayList : public List<E>
 		int current_;
 
 		// Array holding list elements
-		E* list_array_;
-
+		T* list_array_;
 };
 
 } // namespace Lists
--- Voltron/Source/DataStructures/Lists/List.h
+++ Voltron/Source/DataStructures/Lists/List.h
@@ -4,6 +4,7 @@
 /// \brief Classes and functions demonstrating list as an Abstract Data Type.
 /// \ref Data Structures and Algorithm Analysis in C++, 3rd. Ed.. Dr. Clifford
 /// A. Shaffer. Fig. 4.1. The ADT for a list.
+/// \ref https://github.com/OpenDSA/OpenDSA/blob/master/SourceCode/C%2B%2B_Templates/Lists/List.h
 //-----------------------------------------------------------------------------
 #ifndef DATA_STRUCTURES_LISTS_LIST_H
 #define DATA_STRUCTURES_LISTS_LIST_H
@@ -16,7 +17,7 @@ namespace Lists
 //-----------------------------------------------------------------------------
 /// \brief List ADT
 //-----------------------------------------------------------------------------
-template <typename E>
+template <typename T>
 class List
 {
   public:
@@ -30,8 +31,8 @@ class List
     //--------------------------------------------------------------------------
     /// \brief Base constructor.
     //--------------------------------------------------------------------------
-    virtual ~List()
-    {}
+    virtual ~List() = default;
+    // {} // Previous implementation:
 
     //--------------------------------------------------------------------------
     /// \brief Clear contents from the list, to make it empty.
@@ -42,19 +43,19 @@ class List
     /// \brief Insert an element at the current location.
     /// item: The element to be inserted.
     //--------------------------------------------------------------------------
-    virtual void insert(const E& item) = 0;
+    virtual void insert(const T& item) = 0;
 
     //--------------------------------------------------------------------------
     /// \brief Append an element at the end of the list.
     /// item: The element to be appended.
     //--------------------------------------------------------------------------
-    virtual void append(const E& item) = 0;
+    virtual void append(const T& item) = 0;
 
     //--------------------------------------------------------------------------
     /// \brief Remove and return the current element.
     /// \return: the element that was removed.
     //--------------------------------------------------------------------------
-    virtual E remove() = 0;
+    virtual T remove() = 0;
 
     //--------------------------------------------------------------------------
     /// \brief Set the current position to the start of the list.
@@ -92,12 +93,24 @@ class List
     /// \brief Set current position.
     /// pos: The position to make current.
     //--------------------------------------------------------------------------
-    virtual void move_to_position(int position) const = 0;
+    virtual void move_to_position(int position) = 0;
+
+    //--------------------------------------------------------------------------
+    /// \brief Return true if current position is at end of the list.
+    //--------------------------------------------------------------------------
+    //virtual bool is_at_end() = 0;
 
     //--------------------------------------------------------------------------
     /// \brief Return: The current element.
     //--------------------------------------------------------------------------
-    virtual const E& get_value() const = 0;
+    virtual const T& get_value() const = 0;
+
+    //--------------------------------------------------------------------------
+    /// \brief Return: The current element.
+    //--------------------------------------------------------------------------
+    //virtual T get_value() const = 0;
+
+    //virtual bool is_empty() = 0;
 
   private:
 
