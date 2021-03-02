605c09ce8178517c4e87317a749b568b37823bae
ernestyalumni <ernestyalumni@gmail.com>
2021-03-01 22:40:14 -0800

    Add Array List unit tests

---
+++ Voltron/Source/UnitTests/DataStructures/Lists/ArrayList_tests.cpp
@@ -0,0 +1,22 @@
+#include "DataStructures/Lists/ArrayList.h"
+
+#include <boost/test/unit_test.hpp>
+
+//using DataStructures::Lists::ArrayList;
+
+BOOST_AUTO_TEST_SUITE(DataStructures)
+BOOST_AUTO_TEST_SUITE(Lists)
+BOOST_AUTO_TEST_SUITE(ArrayList_tests)
+
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
+BOOST_AUTO_TEST_CASE(DefaultConstructs)
+{
+  ArrayList<char> array_list {};
+
+  BOOST_TEST(true);
+}
+
+BOOST_AUTO_TEST_SUITE_END() // ArrayList_tests
+BOOST_AUTO_TEST_SUITE_END() // Lists
+BOOST_AUTO_TEST_SUITE_END() // DataStructures
