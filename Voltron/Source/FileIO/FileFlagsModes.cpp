6034d0ad84356c3d1875121e2f370ef0e0bf341e
ernestyalumni <ernestyalumni@gmail.com>
2021-03-05 11:31:36 -0800

    Added FileFlagsModes.cpp that was missing

---
+++ Voltron/Source/FileIO/FileFlagsModes.cpp
@@ -0,0 +1,21 @@
+#include "FileFlagsModes.h"
+
+#include "Cpp/Utilities/TypeSupport/UnderlyingTypes.h"
+
+using Cpp::Utilities::TypeSupport::get_underlying_value;
+
+namespace FileIO
+{
+
+int to_access_mode_value(AccessMode access_mode)
+{
+  return get_underlying_value<AccessMode>(access_mode);
+}
+
+int to_file_flag_value(FileFlag file_flag)
+{
+  return get_underlying_value<FileFlag>(file_flag);
+}
+
+
+} // namespace FileIO
