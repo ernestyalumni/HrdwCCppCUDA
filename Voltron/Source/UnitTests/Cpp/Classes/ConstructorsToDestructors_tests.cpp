99ab7c5baa88ce9a13e73bc42bcb5d1d76419fe7
ernestyalumni <ernestyalumni@gmail.com>
2021-03-06 16:06:30 -0800

    Started adding unit tests for constructors to destructors

---
+++ Voltron/Source/UnitTests/Cpp/Classes/ConstructorsToDestructors_tests.cpp
@@ -0,0 +1,18 @@
+#include "Cpp/Classes/ConstructorsToDestructors.h"
+
+#include <boost/test/unit_test.hpp>
+
+//using Cpp::Classes::RuleOf5Object;
+
+BOOST_AUTO_TEST_SUITE(Hierarchy)
+BOOST_AUTO_TEST_SUITE(ConstructorsToDestructors_tests)
+
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
+BOOST_AUTO_TEST_CASE(DefaultConstruction)
+{
+  BOOST_TEST(false);
+}
+
+BOOST_AUTO_TEST_SUITE_END() // ConstructorsToDestructors_tests
+BOOST_AUTO_TEST_SUITE_END() // Cpp
