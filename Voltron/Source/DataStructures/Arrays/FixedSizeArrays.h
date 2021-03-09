c0756efa166a0784307f92cbdeaec87979346133
Ernest Yeung <ernestyalumni@gmail.com>
2021-03-09 01:06:56 -0800

    Start adding FixedSizeArraysOnStack

--- Voltron/Source/DataStructures/Arrays/FixedSizeArrays.h
+++ Voltron/Source/DataStructures/Arrays/FixedSizeArrays.h
@@ -13,6 +13,7 @@
 #include <cstddef> // std::size_t
 #include <stdexcept> // std::out_of_range, std::runtime_error
 #include <string>
+#include <type_traits> // std::enable_if_t
 
 namespace DataStructures
 {
@@ -71,7 +72,13 @@ class DynamicFixedSizeArray : Array<T>
 					"DynamicFixedSizeArray ctor: Invalid input N:" + std::to_string(N));
 			}
 
-			data_ = data;
+			// Copy the data rather than copy the pointer, in fear of input pointer
+			// data being deleted elsewhere.
+
+			for (size_t index {0}; index < size_; ++index)
+			{
+				data_[index] = data[index];
+			}
 		}
 
 		// Copies, Moves.
@@ -102,7 +109,9 @@ class DynamicFixedSizeArray : Array<T>
 			for (size_t index {0}; index < size_; ++index)
 			{
 				data_[index] = other[index];
-			}		
+			}
+
+			return *this;
 		}
 
 		// Move ctor.
@@ -110,6 +119,8 @@ class DynamicFixedSizeArray : Array<T>
 			data_{other.data_},
 			size_{other.size_}
 		{
+			other.data_ = nullptr;
+
 			// So not to invoke delete in dtor of other.
 			other.size_ = 0;
 		}
@@ -129,6 +140,7 @@ class DynamicFixedSizeArray : Array<T>
 			data_ = other.data_;
 			other.size_ = 0;
 
+			other.data_ = nullptr;
 			return *this;
 		}
 
@@ -183,6 +195,18 @@ class DynamicFixedSizeArray : Array<T>
 			return size_;
 		}
 
+		// Returns an iterator to the beginning.
+		constexpr T* begin()
+		{
+			return data_;
+		}
+
+		// Returns an iterator to the end.
+		constexpr T* end()
+		{
+			return data_ + size_;
+		}
+
 		virtual size_t alignment_in_bytes() const
 		{
 			return alignof(DynamicFixedSizeArray);
@@ -193,6 +217,197 @@ class DynamicFixedSizeArray : Array<T>
 		T* data_;
 		size_t size_;
 };
+
+template <std::size_t N, typename T, std::enable_if_t<(N > 0)>* = nullptr>
+class FixedSizeArrayOnStack : Array<T>
+{
+	public:
+
+		using size_t = std::size_t;
+
+		FixedSizeArrayOnStack():
+			data_{},
+			size_{N}
+		{}
+
+		explicit FixedSizeArrayOnStack(const std::initializer_list<T> list):
+			data_{},
+			size_{list.size()}
+		{
+			if (list.size() != N)
+			{
+				std::runtime_error(
+					"FixedSizeArrayOnStack ctor; mismatching sizes; list size: " +
+					std::to_string(list.size()) +
+					"N: " +
+					std::to_string(N));
+			}
+
+			T* data_ptr {data_};
+
+			for (auto x : list)
+			{
+				*data_ptr = x;
+				++data_ptr;
+			}
+		}
+
+		explicit FixedSizeArrayOnStack(T* data, const size_t size):
+			data_{},
+			size_{size}
+		{
+			if (N != size)
+			{
+				std::runtime_error(
+					"FixedSizeArrayOnStack ctor: Invalid input size:" +
+					std::to_string(size) +
+					"N: " +
+					std::to_string(N));
+			}
+
+			data_ = data;
+		}
+
+		// TODO: Fill this in.
+		// Copies, Moves.
+		/*
+		// Copy ctor.
+		DynamicFixedSizeArray(const DynamicFixedSizeArray& other):
+			data_{new T[other.size()]},
+			size_{other.size_}
+		{
+			for (size_t index {0}; index < size_; ++index)
+			{
+				data_[index] = other[index];
+			}
+		}
+
+		// Copy assignment.
+		DynamicFixedSizeArray& operator=(const DynamicFixedSizeArray& other)
+		{
+			if (this->size() != other.size())
+			{
+				std::runtime_error(
+					"Mismatching sizes - this size:" +
+					std::to_string(this->size()) +
+					"other size: " +
+					std::to_string(other.size()));
+			}
+
+			for (size_t index {0}; index < size_; ++index)
+			{
+				data_[index] = other[index];
+			}
+
+			return *this;
+		}
+
+		// Move ctor.
+		DynamicFixedSizeArray(DynamicFixedSizeArray&& other):
+			data_{other.data_},
+			size_{other.size_}
+		{
+			other.data_ = nullptr;
+
+			// So not to invoke delete in dtor of other.
+			other.size_ = 0;
+		}
+
+		// Move assignment.
+		DynamicFixedSizeArray& operator=(DynamicFixedSizeArray&& other)
+		{
+			if (this->size() != other.size())
+			{
+				std::runtime_error(
+					"Mismatching sizes - this size:" +
+					std::to_string(this->size()) +
+					"other size: " +
+					std::to_string(other.size()));
+			}
+
+			data_ = other.data_;
+			other.size_ = 0;
+
+			other.data_ = nullptr;
+			return *this;
+		}
+
+		~DynamicFixedSizeArray()
+		{
+			if (size() > 0)
+			{
+				delete[] data_;
+			}
+		}
+
+		virtual const T& get_value(const int index) const
+		{
+			if (index >= size_ || index > 0)
+			{
+				throw std::out_of_range(
+					"Out of Range, DynamicFixedSizeArray: index input:" +
+						std::to_string(index) +
+						" size: " +
+						std::to_string(size_));
+			}
+
+			return data_[index];
+		}
+
+		virtual void set_value(const int index, const T value)
+		{
+			if (index >= size_ || index > 0)
+			{
+				throw std::out_of_range(
+					"Out of Range, DynamicFixedSizeArray: index input:" +
+						std::to_string(index) +
+						" size: " +
+						std::to_string(size_));
+			}
+
+			data_[index] = value;
+		}
+
+		virtual T& operator[](const size_t index)
+		{
+			return data_[index];
+		}
+
+		virtual const T& operator[](const size_t index) const
+		{
+			return data_[index];
+		}
+
+		virtual size_t size() const
+		{
+			return size_;
+		}
+
+		// Returns an iterator to the beginning.
+		constexpr T* begin()
+		{
+			return data_;
+		}
+
+		// Returns an iterator to the end.
+		constexpr T* end()
+		{
+			return data_ + size_;
+		}
+
+		virtual size_t alignment_in_bytes() const
+		{
+			return alignof(DynamicFixedSizeArray);
+		}
+		*/
+
+	private:	
+
+		T data_[N];
+		size_t size_;
+};
+
+
 
 } // namespace Arrays
 } // namespace DataStructures
--- Voltron/Source/UnitTests/DataStructures/Arrays/FixedSizeArrays_tests.cpp
+++ Voltron/Source/UnitTests/DataStructures/Arrays/FixedSizeArrays_tests.cpp
@@ -2,8 +2,10 @@
 #include "DataStructures/Arrays/FixedSizeArrays.h"
 
 #include <boost/test/unit_test.hpp>
+#include <utility>
 
 using DataStructures::Arrays::DynamicFixedSizeArray;
+using std::move;
 
 BOOST_AUTO_TEST_SUITE(DataStructures)
 BOOST_AUTO_TEST_SUITE(Arrays)
@@ -24,6 +26,95 @@ BOOST_AUTO_TEST_CASE(ConstructsWithInitializerList)
 		BOOST_TEST(a[i] == 5 - i);
 	}
 }
+
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
+BOOST_AUTO_TEST_CASE(CopyConstructs)
+{
+	DynamicFixedSizeArray a {{5, 4, 3, 2, 1}};
+	DynamicFixedSizeArray b {a};
+
+	BOOST_TEST_REQUIRE(a.size() == 5);
+	BOOST_TEST(b.size() == a.size());
+
+	for (int i {0}; i < 5; ++i)
+	{
+		BOOST_TEST(a[i] == 5 - i);
+		BOOST_TEST(b[i] == 5 - i);
+	}
+
+	BOOST_TEST(true);
+}
+
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
+BOOST_AUTO_TEST_CASE(CopyAssigns)
+{
+	DynamicFixedSizeArray a {{5, 4, 3, 2, 1}};
+	DynamicFixedSizeArray b = a;
+
+	BOOST_TEST_REQUIRE(a.size() == 5);
+	BOOST_TEST(b.size() == a.size());
+
+	for (int i {0}; i < 5; ++i)
+	{
+		BOOST_TEST(a[i] == 5 - i);
+		BOOST_TEST(b[i] == 5 - i);
+	}
+}
+
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
+BOOST_AUTO_TEST_CASE(MoveConstructs)
+{
+	DynamicFixedSizeArray a {{5, 4, 3, 2, 1}};
+	DynamicFixedSizeArray b {move(a)};
+
+	BOOST_TEST(a.size() == 0);
+	BOOST_TEST(b.size() == 5);
+
+	for (int i {0}; i < 5; ++i)
+	{
+		BOOST_TEST(b[i] == 5 - i);
+	}
+
+	BOOST_TEST(true);
+}
+
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
+BOOST_AUTO_TEST_CASE(MoveAssigns)
+{
+	DynamicFixedSizeArray a {{5, 4, 3, 2, 1}};
+	DynamicFixedSizeArray b = move(a);
+
+	BOOST_TEST(a.size() == 0);
+	BOOST_TEST(b.size() == 5);
+
+	for (int i {0}; i < 5; ++i)
+	{
+		BOOST_TEST(b[i] == 5 - i);
+	}
+}
+
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
+BOOST_AUTO_TEST_CASE(SizesAfterMoveConstructs)
+{
+	DynamicFixedSizeArray a {{5, 4, 3, 2, 1}};
+	DynamicFixedSizeArray b {move(a)};
+
+	BOOST_TEST(a.size() == 0);
+	BOOST_TEST(b.size() == 5);
+
+	for (int i {0}; i < 5; ++i)
+	{
+		BOOST_TEST(b[i] == 5 - i);
+	}
+
+	BOOST_TEST(true);
+}
+
 
 BOOST_AUTO_TEST_SUITE_END() // DynamicFixedSizeArray_tests
 
