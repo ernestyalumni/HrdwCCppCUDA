3dc0ff9da4f82a4e7f288a02f9e61eb1e22ae23b
ernestyalumni <ernestyalumni@gmail.com>
2021-09-19 14:32:48 -0700

    Added reference to UDP example, added notes on tries, added memoization notes.

---
+++ Voltron/Source/UnitTests/Algorithms/DynamicProgramming/Memoization_tests.cpp
@@ -0,0 +1,16 @@
+#include <boost/test/unit_test.hpp>
+
+BOOST_AUTO_TEST_SUITE(Algorithms)
+BOOST_AUTO_TEST_SUITE(DynamicProgramming)
+BOOST_AUTO_TEST_SUITE(Memoization_tests)
+
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
+BOOST_AUTO_TEST_CASE(DemonstrateStraightforwardAlgorithm)
+{
+  BOOST_TEST(true);
+}
+
+BOOST_AUTO_TEST_SUITE_END() // Memoization_tests
+BOOST_AUTO_TEST_SUITE_END() // DynamicProgramming
+BOOST_AUTO_TEST_SUITE_END() // Algorithms
