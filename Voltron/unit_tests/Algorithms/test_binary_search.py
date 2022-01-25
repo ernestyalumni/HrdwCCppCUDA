97d5eefc5f4fead167a16ce8d96c59c839843506
Ernest Yeung <ernestyalumni@gmail.com>
2022-01-25 01:11:07 -0800

    Added more tests to binary search

--- Voltron/unit_tests/Algorithms/test_binary_search.py
+++ Voltron/unit_tests/Algorithms/test_binary_search.py
@@ -110,6 +110,23 @@ def test_binary_search_recursive_works():
     assert result == None
 
 
+def test_binary_search_recursive_works_on_all_elements():
+    # cf. https://www.geeksforgeeks.org/binary-search/
+
+    array = [2, 5, 8, 12, 16, 23, 38, 56, 72, 91]
+
+    assert binary_search_recursive(array, 2, 0, len(array) - 1) == 0
+    assert binary_search_recursive(array, 5, 0, len(array) - 1) == 1
+    assert binary_search_recursive(array, 8, 0, len(array) - 1) == 2
+    assert binary_search_recursive(array, 12, 0, len(array) - 1) == 3
+    assert binary_search_recursive(array, 16, 0, len(array) - 1) == 4
+    assert binary_search_recursive(array, 23, 0, len(array) - 1) == 5
+    assert binary_search_recursive(array, 38, 0, len(array) - 1) == 6
+    assert binary_search_recursive(array, 56, 0, len(array) - 1) == 7
+    assert binary_search_recursive(array, 72, 0, len(array) - 1) == 8
+    assert binary_search_recursive(array, 91, 0, len(array) - 1) == 9
+
+
 def test_binary_search_iterative_works():
 
     element = 18
@@ -129,3 +146,19 @@ def test_binary_search_iterative_works():
     result = binary_search_iterative(array, element)
 
     assert result == None
+
+
+def test_binary_search_iterative_works_on_all_elements():
+
+    array = [2, 5, 8, 12, 16, 23, 38, 56, 72, 91]
+
+    assert binary_search_iterative(array, 2) == 0
+    assert binary_search_iterative(array, 5) == 1
+    assert binary_search_iterative(array, 8) == 2
+    assert binary_search_iterative(array, 12) == 3
+    assert binary_search_iterative(array, 16) == 4
+    assert binary_search_iterative(array, 23) == 5
+    assert binary_search_iterative(array, 38) == 6
+    assert binary_search_iterative(array, 56) == 7
+    assert binary_search_iterative(array, 72) == 8
+    assert binary_search_iterative(array, 91) == 9
