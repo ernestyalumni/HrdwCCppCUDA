2c4ffde054edac9c9884614aef6e55ff5350cb16
ernestyalumni <ernestyalumni@gmail.com>
2022-06-06 23:59:56 -0700

    Added Nlohmann json through cmake fetch

---
+++ Constructicon/Source/UnitTests/Dependencies/json_tests.cpp
@@ -0,0 +1,22 @@
+#include "nlohmann/json.hpp"
+#include <gtest/gtest.h>
+
+using json = nlohmann::json;
+
+namespace GoogleUnitTests
+{
+namespace Dependencies
+{
+namespace Json
+{
+
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
+TEST(JsonTests, Constructs)
+{
+
+}
+
+} // namespace Json
+} // namespace Dependencies
+} // namespace GoogleUnitTests
