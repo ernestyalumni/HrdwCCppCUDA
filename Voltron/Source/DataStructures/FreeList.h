68a4a4e0118b404243665854d515fdcc3c137c77
ernestyalumni <ernestyalumni@gmail.com>
2022-11-12 19:26:07 -0800

    Added FreeList implementation

--- Voltron/Source/DataStructures/FreeList.h
+++ Voltron/Source/DataStructures/FreeList.h
@@ -22,10 +22,10 @@ class FreeList
 
     inline static constexpr std::size_t default_size_ {32};
 
-    using ListType =
+    using List_T =
       DataStructures::LinkedLists::DoublyLinkedList<StaticFreeList<T>>;
     using Item = StaticFreeList<T>::Item;
-    using ITERATOR_T = ListType::Iterator;
+    using Iterator_T = List_T::Iterator;
 
     FreeList(const std::size_t initial_size = default_size_):
       block_size_{
@@ -34,8 +34,10 @@ class FreeList
           std::min<std::size_t>(initial_size, max_block_size_))}
     {}
 
-    FreeList(const& FreeList) = delete;
-    FreeList& operator=(const& FreeList) = delete;
+    FreeList(const FreeList&) = delete;
+    FreeList& operator=(const FreeList&) = delete;
+
+    ~FreeList() = default;
 
     //--------------------------------------------------------------------------
     /// \ref pp. 50, Ch. 5 Fundamental Data Structures of D. Kedyk, Implementing
@@ -46,8 +48,8 @@ class FreeList
     //--------------------------------------------------------------------------
     T* allocate()
     {
-      ListType::Iterator first {blocks_.begin()};
-      if (first == blocks_.end() || first->is_full())
+      Iterator_T first {blocks_.begin()};
+      if (first == blocks_.end() || first->retrieve().is_full())
       {
         // Make new first block if needed.
         blocks_.push_front(block_size_);
@@ -55,9 +57,9 @@ class FreeList
         block_size_ = std::min<std::size_t>(block_size_ * 2, max_block_size_);
       }
 
-      Item* result {first_->allocate()};
+      Item* result {first->value_.allocate()};
       // Block list pointer.
-      result->user_data_ = (void*)first_.get_current();
+      result->user_data_ = (void*)first.get_current();
 
       // Move full blocks to the end.
       if (first->is_full())
@@ -75,12 +77,12 @@ class FreeList
       // Handle null pointer.
       if (!item)
       {
-        return item;
+        return;
       }
       // Cast back from first member.
       Item* node {reinterpret_cast<Item*>(item)};
-      ITERATOR_T came_from {
-        reinterpret_cast<ListType::Node*>(node->user_data_)};
+      Iterator_T came_from {
+        reinterpret_cast<List_T::Node*>(node->user_data_)};
 
       came_from->list_delete(node);
 
@@ -90,7 +92,7 @@ class FreeList
         // Beware that block boundary delete/remove thrashes, but unlikely.
         block_size_ = std::max<std::size_t>(
           min_block_size_,
-          block_size - came_from->capacity_);
+          block_size_ - came_from->capacity_);
         blocks_.list_delete(came_from);
       }
       // Available blocks to the front.
@@ -100,11 +102,15 @@ class FreeList
       }
     }
 
+    std::size_t get_block_size() const
+    {
+      return block_size_;
+    }
+
   private:
 
     std::size_t block_size_;
-    ListType blocks_;
-
+    List_T blocks_;
 };
 
 } // namespace Kedyk
---
+++ Voltron/Source/UnitTests/DataStructures/FreeList_tests.cpp
@@ -0,0 +1,41 @@
+#include "DataStructures/FreeList.h"
+
+#include <boost/test/unit_test.hpp>
+#include <string>
+
+BOOST_AUTO_TEST_SUITE(DataStructures)
+
+BOOST_AUTO_TEST_SUITE(Kedyk)
+
+BOOST_AUTO_TEST_SUITE(FreeList_tests)
+
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
+BOOST_AUTO_TEST_CASE(DefaultConstructs)
+{
+  FreeList<std::string> fl {};
+
+  BOOST_TEST(fl.get_block_size() == 32);
+}
+
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
+BOOST_AUTO_TEST_CASE(ConstructsForSmallBlockSize)
+{
+  FreeList<std::string> fl {4};
+
+  BOOST_TEST(fl.get_block_size() == 8);
+}
+
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
+BOOST_AUTO_TEST_CASE(AllocateAllocates)
+{
+  FreeList<int> fl {8};
+  int* allocated {fl.allocate()};
+}
+
+BOOST_AUTO_TEST_SUITE_END() // FreeList_tests
+BOOST_AUTO_TEST_SUITE_END() // Kedyk
+
+BOOST_AUTO_TEST_SUITE_END() // DataStructures
