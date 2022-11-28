aa405a82442151af55e0789238d2808c65a5fa51
Ernest Yeung <ernestyalumni@gmail.com>
2022-11-28 01:21:41 -0800

    Add modules exampleas DataStructures

---
+++ Constructicon/Mixmaster/projects/DataStructures/src/lib.rs
@@ -0,0 +1,32 @@
+//-------------------------------------------------------------------------------------------------
+/// \ref https://doc.rust-lang.org/stable/book/ch07-02-defining-modules-to-control-scope-and-privacy.html
+/// 7.2 Defining Modules to Control Scope and Privacy.
+/// \details When compiling a crate, compiler first looks in the crate root file (usually
+/// src/lib.rs for library crate, src/main.rs for binary crate) for code to compile.
+/// In crate root file, declare new modules.
+/// Compiler will look for module's code in these places:
+/// * inline, within curly brackets taht replace semicolon following mod module_name
+/// * In file src/module_name.rs
+/// * In file src/module_name/mod.rs
+/// In an file other than crate root, you can declare submodules.
+//-------------------------------------------------------------------------------------------------
+
+// Code within a module is private from its parent modules by default. To make a module public,
+// declare it with `pub mod` instead of `mod`.
+
+pub mod Arrays;
+
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
