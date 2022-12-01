899bd6456f22f4742fdb7649cfd48e28f58783c1
ernestyalumni <ernestyalumni@gmail.com>
2022-11-30 23:16:43 -0800

    Edited the exiting option

--- Constructicon/Mixmaster/Setup.sh
+++ Constructicon/Mixmaster/Setup.sh
@@ -1,14 +1,18 @@
 #/bin/bash
 
 # Make this an executable with `chmod` or run with `bash Setup.sh`
-
 # cf. https://stackoverflow.com/questions/226703/how-do-i-prompt-for-yes-no-cancel-input-in-a-linux-shell-script
+
+# cf. https://github.com/rust-lang/rustup/issues/686
+# Once you're done, you want to do
+# source ~/.profile
 
 echo "Do you wish to install rust?"
 select yn in "Yes" "No"; do
     case $yn in
         Yes ) curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh; break;;
-        No ) exit;;
+        No ) break;
+#        No ) exit;;
     # cf. https://unix.stackexchange.com/questions/256149/what-does-esac-mean-at-the-end-of-a-bash-case-statement-is-it-required
     # esac is backwards for case and is proper way to close case.
     esac
