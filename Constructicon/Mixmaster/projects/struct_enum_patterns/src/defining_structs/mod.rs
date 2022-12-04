a412acb12bd287f10c5b0712cd55c7d29f1ae354
Ernest Yeung <ernestyalumni@gmail.com>
2022-12-04 09:33:47 -0800

    Added more examples of struct and starting enums

--- Constructicon/Mixmaster/projects/struct_enum_patterns/src/defining_structs/mod.rs
+++ Constructicon/Mixmaster/projects/struct_enum_patterns/src/defining_structs/mod.rs
@@ -35,9 +35,48 @@ pub struct Color(pub i32, pub i32, pub i32);
 
 pub struct Point(pub i32, pub i32, pub i32);
 
-
+// outer attribute, Debug trait, enables us to print out struct in a way that's useful for
+// developers
 #[derive(Debug)]
 pub struct Rectangle {
   pub width: u32,
   pub height: u32,
+}
+
+//-------------------------------------------------------------------------------------------------
+/// Method Syntax
+/// Methods are defined within context of struct (or enum or trait object), and their first
+/// parameter is always self, which represents the instance of struct the method is being called
+/// on.
+/// To define function within the context of Rectangle, start an impl (implementation) block for
+/// Rectangle. Everything within this impl block will be associated with the Rectangle type.
+///
+/// All functions defined within impl block are called associated functions because they're
+/// associated with type named after impl.
+//-------------------------------------------------------------------------------------------------
+impl Rectangle
+{
+  // &self is actually short for self: &Self, type Self is an alias for the type that the impl
+  // block is for.
+  pub fn area(&self) -> u32
+  {
+    self.width * self.height
+  }
+
+  pub fn can_hold(&self, other: &Rectangle) -> bool
+  {
+    self.width > other.width && self.height > other.height
+  }  
+}
+
+// Each struct is allowed to have multiple impl blocks.
+impl Rectangle
+{
+  pub fn square(size: u32) -> Self {
+    Self
+    {
+      width: size,
+      height: size,
+    }
+  }
 }
--- Constructicon/Mixmaster/projects/struct_enum_patterns/src/main.rs
+++ Constructicon/Mixmaster/projects/struct_enum_patterns/src/main.rs
@@ -1,44 +1,78 @@
 pub mod defining_structs;
 
-fn main() {
-    // Create an instance of the struct by specifying concrete values for each fields.
-    let user1 = defining_structs::User {
-        email: String::from("someone@example.com"),
-        username: String::from("someusername123"),
-        active: true,
-        sign_in_count: 1,
-    };
+fn main()
+{
+  // Create an instance of the struct by specifying concrete values for each fields.
+  let user1 = defining_structs::User {
+      email: String::from("someone@example.com"),
+      username: String::from("someusername123"),
+      active: true,
+      sign_in_count: 1,
+  };
 
-    println!("user1: {} ", user1.email);
+  println!("user1: {} ", user1.email);
 
-    println!("Hello, world!");
+  println!("Hello, world!");
 
-    let user2 = defining_structs::build_user(
-        String::from("some2@example.com"),
-        String::from("created"));
+  let user2 = defining_structs::build_user(
+      String::from("some2@example.com"),
+      String::from("created"));
 
-    println!("dear god: {}, {}", user2.username, user2.sign_in_count);
+  println!("dear god: {}, {}", user2.username, user2.sign_in_count);
 
-    let user3 = defining_structs::build_user2(
-        String::from("some3@example.com"),
-        String::from("created3"));
+  let user3 = defining_structs::build_user2(
+      String::from("some3@example.com"),
+      String::from("created3"));
 
-    println!("dear god: {}", user3.username);
+  println!("dear god: {}", user3.username);
 
-    let new_user3 = defining_structs::User {
-        email: String::from("another3@example.com"),
-        // ..user3 must come last to specify that any remaining fields should get their values from
-        // the corresponding fields in user3.
-        ..user3
-    };
+  let new_user3 = defining_structs::User {
+      email: String::from("another3@example.com"),
+      // ..user3 must come last to specify that any remaining fields should get their values from
+      // the corresponding fields in user3.
+      ..user3
+  };
 
-    println!("gift subs: {} {}", new_user3.email, new_user3.username);
+  println!("gift subs: {} {}", new_user3.email, new_user3.username);
 
-    let black = defining_structs::Color(0, 1, 2);
+  let black = defining_structs::Color(0, 1, 2);
 
-    let origin = defining_structs::Point(2, 1, 0);
+  let origin = defining_structs::Point(2, 1, 0);
 
-    println!("black {} {} ", black.0, black.2);
-    println!("origin {} {} ", origin.1, origin.2);
+  println!("black {} {} ", black.0, black.2);
+  println!("origin {} {} ", origin.1, origin.2);
 
+  let rect1 = defining_structs::Rectangle {
+    width: 30,
+    height: 50,
+  };
+
+  println!{" with no hash {:?}", rect1};
+
+  // Useful format for large structs.
+  println!(" with hash {:#?}", rect1);
+
+  // dbg! macro prints to standard error console.
+  dbg!(&rect1);
+
+  println!("The area of the rectangle is {} square pixels.", rect1.area());
+
+  let rect2 = defining_structs::Rectangle
+  {
+    width: 10,
+    height: 40,
+  };
+
+  let rect3 = defining_structs::Rectangle
+  {
+    width: 60,
+    height: 45,
+  };
+
+  println!("Can rect1 hold rect2 {}", rect1.can_hold(&rect2));
+  println!("Can rect1 hold rect3 {}", rect1.can_hold(&rect3));
+
+  let rect4 = defining_structs::Rectangle::square(3);
+
+  println!("Area of square is {}", rect4.area());
 }
