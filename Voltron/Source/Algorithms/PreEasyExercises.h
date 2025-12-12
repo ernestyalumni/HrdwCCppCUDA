4614ce46626185dffd576d028f4eff6f4b30b787
Ernest Yeung <ernestyalumni@gmail.com>
2025-12-12 08:28:12 -0800

    Added more preeasy array iteration

--- Voltron/Source/Algorithms/PreEasyExercises.h
+++ Voltron/Source/Algorithms/PreEasyExercises.h
@@ -1,12 +1,69 @@
 #ifndef ALGORITHMS_PRE_EASY_EXERCISES_H
 #define ALGORITHMS_PRE_EASY_EXERCISES_H
 
-#include <concepts>
+#include <concepts> // std::integral
 #include <iostream>
+#include <unordered_map>
 #include <vector>
 
 namespace Algorithms
 {
+
+//------------------------------------------------------------------------------
+/// https://en.cppreference.com/w/cpp/concepts/integral.html
+/// The concept integral<T> is satisfied if and only if T is an integral type.
+/// O(sqrt(n)) time.
+//------------------------------------------------------------------------------
+template <typename T>
+bool is_prime(const T n) requires std::integral<T>
+{
+
+  if (n <= 1)
+  {
+    return false;
+  }
+
+  if (n <= 3)
+  {
+    return true;
+  }
+
+  // Check divisibility by all numbers from 2 to the square root of n.
+  for (T i {2}; i * i <= n; ++i)
+  {
+    if (n % i == 0)
+    {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+//------------------------------------------------------------------------------
+/// O(sqrt(n)) time. O(n) space.
+//------------------------------------------------------------------------------
+template <typename T>
+bool is_prime_with_memoization(const T n) requires std::integral<T>
+{
+  static std::unordered_map<T, bool> memo {};
+  if (memo.find(n) != memo.end())
+  {
+    return memo[n];
+  }
+
+  if (is_prime(n))
+  {
+    memo[n] = true;
+    return true;
+  }
+  else
+  {
+    memo[n] = false;
+    return false;
+  }
+}
+
 namespace PreEasyExercises
 {
 
@@ -67,6 +124,203 @@ class ArrayIndexing
       }
       return result;
     }
+
+    //--------------------------------------------------------------------------
+    /// 3. Fetch Every Second Element
+    /// Write a function that accesses every other element in the array,
+    /// starting from the first element.
+    /// O(n/2) time, for visiting every second element.
+    //--------------------------------------------------------------------------
+    template <std::ranges::range Container>
+    static auto fetch_second_element(
+      const Container& array,
+      const bool is_print=false)
+    {
+      Container result {};
+
+      auto iter = std::ranges::begin(array);
+      const auto end = std::ranges::end(array);
+      while (iter != end)
+      {
+        result.push_back(*iter);
+        if (is_print)
+        {
+          std::cout << *iter << ' ';
+        }
+
+        // https://en.cppreference.com/w/cpp/iterator/ranges/next
+        // constexpr I next(I i, std::iter_difference_t<I> n, S bound);
+        // i - an iterator
+        // n - number of elements to advance
+        // bound - sentinel denoting end of range i points to
+        iter = std::ranges::next(iter, 2, end);  
+      }
+      return result;
+    }
+
+    //--------------------------------------------------------------------------
+    /// 4. Find the Index of a Target Element
+    /// Write a function that searches for a specific element in an array and
+    /// returns its index. If the element is not found, return -1.
+    //--------------------------------------------------------------------------
+    template <std::ranges::input_range Container, typename T>
+    static int find_index_of_target_element(
+      const Container& array,
+      const T target,
+      const bool is_print=false)
+    {
+      std::size_t index {0};
+      for (const auto& element : array)
+      {
+        if (element == target)
+        {
+          return static_cast<int>(index);
+        }
+        ++index;
+      }
+      return -1;
+    }
+
+    //--------------------------------------------------------------------------
+    /// 5. Find the First Prime Number in an Array
+    /// Iterate over an array and find the first prime number. Stop the
+    /// iteration once you find it.
+    //--------------------------------------------------------------------------
+    template <std::ranges::range Container>
+    static std::ranges::range_value_t<Container> find_first_prime_number(
+      const Container& array) requires std::integral<
+        std::ranges::range_value_t<Container>>
+    {
+      for (const auto element : array)
+      {
+        if (is_prime(element))
+        {
+          return element;
+        }
+      }
+      return -1;
+    }
+
+    //--------------------------------------------------------------------------
+    /// 6. Traverse a Two-Dimensional Array
+    ///
+    /// Write a function to print all elements of a 2D array (matrix), row by
+    /// row.
+    //--------------------------------------------------------------------------
+    template <std::ranges::input_range OuterContainer>
+    static std::vector<
+      std::ranges::range_value_t<
+        std::ranges::range_value_t<
+          OuterContainer>>> traverse_two_dimensional_array(
+      const OuterContainer& array,
+      const bool is_print = false)
+    {
+      using ElementType = std::ranges::range_value_t<
+        std::ranges::range_value_t<OuterContainer>>;
+      std::vector<ElementType> result {};
+
+      for (const auto& row : array)
+      {
+        for (const auto element : row)
+        {
+          result.push_back(element);
+          if (is_print)
+          {
+            std::cout << element << ' ';
+          }
+        }
+      }
+      return result;
+    }
+
+    //--------------------------------------------------------------------------
+    /// 7. Traverse the Main Diagonal of a Matrix
+    /// Print the elements along the main diagonal of a square matrix, where the
+    /// row and column indices are equal.
+    //--------------------------------------------------------------------------
+    template <std::ranges::input_range OuterContainer>
+    static std::vector<
+      std::ranges::range_value_t<
+        std::ranges::range_value_t<OuterContainer>>> traverse_main_diagonal(
+        const OuterContainer& array,
+        const bool is_print = false)
+    {
+      using ElementType = std::ranges::range_value_t<
+        std::ranges::range_value_t<OuterContainer>>;
+      std::vector<ElementType> result {};
+
+      const auto M = std::ranges::size(array);
+
+      if (M == 0)
+      {
+        return result;
+      }
+      const auto N = std::ranges::size(array[0]);
+      for (std::size_t i {0}; i < std::min(M, N); ++i)
+      {
+        result.push_back(array[i][i]);
+        if (is_print)
+        {
+          std::cout << array[i][i] << ' ';
+        }
+      }
+      return result;
+    }
+
+    //--------------------------------------------------------------------------
+    /// 8. Traverse the Perimeter of a Matrix
+    /// Print the elements along the outer edge (perimeter) of a 2D array.
+    //--------------------------------------------------------------------------
+    template <typename Container, typename T>
+    static std::vector<T> traverse_perimeter(
+      const Container& array,
+      const bool is_print = false)
+    {
+      std::vector<T> result {};
+      const int M {static_cast<int>(std::ranges::size(array))};
+      if (M == 0)
+      {
+        return result;
+      }
+
+      const int N {static_cast<int>(std::ranges::size(array[0]))};
+      int m_up {0};
+      int m_down {M - 1};
+      int n_left {0};
+      int n_right {N - 1};
+
+      auto is_traverse_complete =
+        [](int m_up, int m_down, int n_left, int n_right) -> bool
+        {
+          return m_up > m_down || n_left > n_right;
+        };
+
+      while (!is_traverse_complete(m_up, m_down, n_left, n_right))
+      {
+        // Traverse top row, stopping before "right" column.
+        for (int j {n_left}; j < n_right; ++j)
+        {
+          result.push_back(array[m_up][j]);
+          if (is_print)
+          {
+            std::cout << array[m_up][j] << ' ';
+          }
+        }
+        ++m_up;
+
+        // Traverse right column, stopping before "down" row.
+        for (int i {m_up}; i < m_down; ++i)
+        {
+          result.push_back(array[i][n_right]);
+          if (is_print)
+          {
+            std::cout << array[i][n_right] << ' ';
+          }
+        }
+      }
+
+    }
+
 };
 
 } // namespace PreEasyExercises
--- Voltron/Source/UnitTests/Algorithms/PreEasyExercises_tests.cpp
+++ Voltron/Source/UnitTests/Algorithms/PreEasyExercises_tests.cpp
@@ -18,11 +18,22 @@ BOOST_AUTO_TEST_SUITE(PreEasyExercises)
 
 BOOST_AUTO_TEST_SUITE(ArrayIndexing_tests)
 
+vector<int> array {1, 2, 3, 4, 5};
+vector<int> array_2 {4, 7, 9, 1};
+vector<int> all_same {5, 5, 5, 5};
+
+vector<vector<int>> matrix {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+vector<vector<int>> matrix_2 {
+  {11, 12, 13, 14},
+  {21, 22, 23, 24},
+  {31, 32, 33, 34},
+  {41, 42, 43, 44}
+};
+
 //------------------------------------------------------------------------------
 //------------------------------------------------------------------------------
 BOOST_AUTO_TEST_CASE(IterateOverArrayIteratesOverAnArray)
 {
-  vector<int> array {1, 2, 3, 4, 5};
   BOOST_TEST(ArrayIndexing::iterate_over_array(array) == array);
 
   // ArrayIndexing::iterate_over_array(array, true);
@@ -35,13 +46,95 @@ BOOST_AUTO_TEST_CASE(IterateOverArrayIteratesOverAnArray)
 //------------------------------------------------------------------------------
 BOOST_AUTO_TEST_CASE(IterateOverArrayInReverseIteratesOverAnArrayInReverse)
 {
-  vector<int> array {1, 2, 3, 4, 5};
   vector<int> expected_result {5, 4, 3, 2, 1};
 
   BOOST_TEST(
     ArrayIndexing::iterate_over_array_in_reverse(array) == expected_result);
 
   // ArrayIndexing::iterate_over_array_in_reverse(array, true);
+}
+
+//------------------------------------------------------------------------------
+/// 3. Fetch Every Second Element
+///
+/// Write a function that accesses every other element in the array, starting
+/// from the first element.
+//------------------------------------------------------------------------------
+BOOST_AUTO_TEST_CASE(FetchSecondElementFetchesSecondElement)
+{
+  vector<int> expected_result {1, 3, 5};
+  BOOST_TEST(ArrayIndexing::fetch_second_element(array) == expected_result);
+}
+
+//------------------------------------------------------------------------------
+/// 4. Find the Index of a Target Element
+/// 
+/// Write a function that searches for a specific element in an array and
+/// returns its index. If the element is not found, return -1.
+//------------------------------------------------------------------------------
+BOOST_AUTO_TEST_CASE(FindIndexOfTargetElementFindsIndex)
+{
+  {
+    BOOST_TEST(ArrayIndexing::find_index_of_target_element(array_2, 1) == 3);
+    BOOST_TEST(ArrayIndexing::find_index_of_target_element(array_2, 9) == 2);
+    BOOST_TEST(ArrayIndexing::find_index_of_target_element(array_2, 5) == -1);
+  }
+
+  vector<int> empty_array {};
+  BOOST_TEST(ArrayIndexing::find_index_of_target_element(empty_array, 1) == -1);
+
+  vector<int> single_element_array {1};
+  BOOST_TEST(ArrayIndexing::find_index_of_target_element(single_element_array, 1) == 0);
+  BOOST_TEST(ArrayIndexing::find_index_of_target_element(single_element_array, 2) == -1);
+
+  vector<int> duplicates {1, 2, 2, 3, 2, 4};
+  BOOST_TEST(ArrayIndexing::find_index_of_target_element(duplicates, 2) == 1);
+
+  BOOST_TEST(ArrayIndexing::find_index_of_target_element(all_same, 5) == 0);
+}
+
+//------------------------------------------------------------------------------
+/// 5. Find the First Prime Number in an Array
+///
+/// Iterate over an array and find the first prime number. Stop the iteration
+/// once you find it.
+//------------------------------------------------------------------------------
+BOOST_AUTO_TEST_CASE(FindFirstPrimeNumberFindsFirstPrimeNumber)
+{
+  BOOST_TEST(ArrayIndexing::find_first_prime_number(array) == 2);
+  BOOST_TEST(ArrayIndexing::find_first_prime_number(array_2) == 7);
+
+  BOOST_TEST(ArrayIndexing::find_first_prime_number(all_same) == 5);
+}
+
+//------------------------------------------------------------------------------
+/// 6. Traverse a Two-Dimensional Array
+//------------------------------------------------------------------------------
+BOOST_AUTO_TEST_CASE(TraverseTwoDimensionalArrayTraverses)
+{
+  vector<int> expected_result {1, 2, 3, 4, 5, 6, 7, 8, 9};
+  BOOST_TEST(
+    ArrayIndexing::traverse_two_dimensional_array(matrix) == expected_result);
+
+  expected_result = {
+    11, 12, 13, 14, 21, 22, 23, 24, 31, 32, 33, 34, 41, 42, 43, 44};
+  BOOST_TEST(
+    ArrayIndexing::traverse_two_dimensional_array(matrix_2) == expected_result);
+}
+
+//------------------------------------------------------------------------------
+/// 7. Traverse the Main Diagonal of a Matrix
+//------------------------------------------------------------------------------
+BOOST_AUTO_TEST_CASE(TraverseMainDiagonalTraverses)
+{
+  vector<int> expected_result {1, 5, 9};
+
+  BOOST_TEST(
+    ArrayIndexing::traverse_main_diagonal(matrix) == expected_result);
+
+  expected_result = {11, 22, 33, 44};
+  BOOST_TEST(
+    ArrayIndexing::traverse_main_diagonal(matrix_2) == expected_result);
 }
 
 BOOST_AUTO_TEST_SUITE_END() // ArrayIndexing_tests
