75e97bf27265282950d8177502ce2f1b9cdb735d
Ernest Yeung <inserviceofxy@proton.me>
2026-01-04 01:05:57 -0800

    Start Rust version of water container for revisit

--- Constructicon/Mixmaster/mixmaster/src/algorithms/leetcode/medium.rs
+++ Constructicon/Mixmaster/mixmaster/src/algorithms/leetcode/medium.rs
@@ -3,6 +3,8 @@ use std::collections::HashMap;
 /// 3. Longest Substring Without Repeating Characters
 pub struct LengthOfLongestSubstring;
 pub struct ThreeSum;
+/// 11. Container With Most Water
+pub struct ContainerWithMostWater;
 
 //------------------------------------------------------------------------------
 /// 3. Longest Substring Without Repeating Characters
@@ -40,6 +42,26 @@ impl LengthOfLongestSubstring
     max_length.try_into().unwrap()
   }
 }
+
+//------------------------------------------------------------------------------
+/// 11. Container With Most Water
+/// You are given an integer array height of length n. There are n vertical
+/// lines drawn such that the two endpoints of the ith line are (i, 0) and 
+/// (i, height[i]).
+///
+/// Find two lines that together with the x-axis form a container, such that the
+/// container contains the most water.
+///
+/// Return the maximum amount of water a container can store.
+///
+/// Notice that you may not slant the container.
+///
+/// Constraints:
+///
+/// * n == height.length
+/// * 2 <= n <= 105
+/// * 0 <= height[i] <= 104
+//------------------------------------------------------------------------------
 
 //------------------------------------------------------------------------------
 /// 15. 3Sum.
